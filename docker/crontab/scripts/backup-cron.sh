b9ac96dd708c4ef5bc38cb79d97ccb4794b4c52c
ioedumac <eacevedof@yahoo.es>
2021-10-25 19:30:52 +0200

    cmabios de poseaf a marketing

--- docker/crontab/scripts/backup-cron.sh
+++ docker/crontab/scripts/backup-cron.sh
@@ -3,4 +3,4 @@
 # /bin/sh /appdata/scripts/backup-cron.sh
 tagnow=$(date '+%Y%m%d-%H%M%S')
 
-mysqldump --host=php-eafpos-db --user=$MYSQL_USER --password=$MYSQL_ROOT_PASSWORD db_eafpos > /appdata/io/out/db_backup_db_eafpos_$tagnow.sql
+mysqldump --host=php-marketing-db --user=$MYSQL_USER --password=$MYSQL_ROOT_PASSWORD db_marketing > /appdata/io/out/db_backup_db_marketing_$tagnow.sql
