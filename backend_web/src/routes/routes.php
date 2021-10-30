6b8257375e859d89712b291bd958b8727d6a0169
ioedumac <eacevedof@yahoo.es>
2021-10-30 15:14:51 +0200

    Views

--- backend_web/src/routes/routes.php
+++ backend_web/src/routes/routes.php
@@ -4,7 +4,7 @@
 
 return [   
     ["url"=>"/","controller"=>"App\Controllers\NotFoundController","method"=>"index"],
-    ["url"=>"/","controller"=>"App\Controllers\NotFoundController","method"=>"index"],
+    ["url"=>"/","controller"=>"App\Controllers\Open\OpenController","method"=>"index"],
     ["url"=>"/logs","controller"=>"App\Controllers\LogsController","method"=>"index"],
     
     ["url"=>"/apify/contexts","controller"=>"App\Controllers\Apify\ContextsController","method"=>"index"],
