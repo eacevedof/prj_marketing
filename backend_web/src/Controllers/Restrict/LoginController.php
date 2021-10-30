0383f5779bf999f56fa88b43ba0f82f1d0079a81
ioedumac <eacevedof@yahoo.es>
2021-10-30 19:59:28 +0200

    restrict controller

---
+++ backend_web/src/Controllers/Restrict/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link eduardoaf.com
+ * @name App\Controllers\Restrict\RestrictController
+ * @file RestrictController.php v1.0.0
+ * @date 30-10-2021 14:33 SPAIN
+ * @observations
+ */
+namespace App\Controllers\Restrict\RestrictController;
+
+
+final class LoginController extends RestrictController
+{
+    public function index(): void
+    {
+
+    }
+
+}//LoginController
