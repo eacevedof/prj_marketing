ef7c6e9061c24ae255888290e443799994fbc341
ioedumac <eacevedof@yahoo.es>
2021-10-31 00:04:44 +0200

    login access swal

--- backend_web/public/assets/js/restrict/login.js
+++ backend_web/public/assets/js/restrict/login.js
@@ -27,15 +27,32 @@ const App = {
           console.log("status",xhr.status)
           if (xhr.status === 200) {
             console.log(xhr.responseText)
-
+            Swal.fire({
+              icon: 'success',
+              title: 'Gracias por contactar conmigo!',
+              html: 'En breves momentos recibirás una copia del mensaje en tu email.',
+            })
           }
           else {
             console.log("xhr error:", xhr);
-
+            Swal.fire({
+              icon: 'warning',
+              title: 'Proceso incompleto',
+              html: 'No se ha podido procesar tu mensaje. Por favor inténtalo más tarde. Disculpa las molestias. <br/>',
+            })
           }
         }
 
       }//onReady
+
+      xhr.onerror = (err) => {
+        console.log("ON ERROR", err)
+        Swal.fire({
+          icon: 'error',
+          title: 'Vaya! Algo ha ido mal (c)',
+          html: 'No se ha podido procesar tu mensaje. Por favor inténtalo más tarde. Disculpa las molestias. \n',
+        })
+      }
 
     }//onSubmit
 
