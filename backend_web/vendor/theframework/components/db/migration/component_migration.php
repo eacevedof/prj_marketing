06688fc7a5bd3e2c5b6259d1134b952f4c7e3ed4
ioedumac <eacevedof@yahoo.es>
2021-10-31 11:39:14 +0100

    todo-migration

--- backend_web/vendor/theframework/components/db/migration/component_migration.php
+++ backend_web/vendor/theframework/components/db/migration/component_migration.php
@@ -2,9 +2,9 @@
 /**
  * @author Eduardo Acevedo Farje.
  * @link eduardoaf.com
- * @name TheFramework\Components\Db\ComponentMssqlExport 
- * @file component_mssql_export.php v2.1.0B
- * @date 30-03-2018 12:06 SPAIN
+ * @name TheFramework\Components\Db\ComponentMigration 
+ * @file component_migration.php v0.0.0
+ * @date 31-10-2021 12:06 SPAIN
  * @observations
  */
 namespace TheFramework\Components\Db\Migration;
