e3cb3b159febdf1dfd28693af71f8c48f56f74c0
ioedumac <eacevedof@yahoo.es>
2021-11-01 23:42:57 +0100

    promociones

--- backend_web/src/Enums/Action.php
+++ backend_web/src/Enums/Action.php
@@ -6,4 +6,6 @@ abstract class Action
 {
     const DASHBOARD_READ = "dashboard:read";
     const DASHBOARD_WRITE = "dashboard:write";
+    const PROMOTIONS_READ = "promotions:read";
+    const PROMOTIONS_WRITE = "promotions:write";
 }
