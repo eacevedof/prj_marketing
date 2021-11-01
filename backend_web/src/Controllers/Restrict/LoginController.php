5ee1bf8c38a8a412a31528c99bfd6a6ce5a8b3f4
ioedumac <eacevedof@yahoo.es>
2021-11-01 19:58:11 +0100

    tratando validacion de email

--- backend_web/src/Controllers/Restrict/LoginController.php
+++ backend_web/src/Controllers/Restrict/LoginController.php
@@ -14,8 +14,6 @@ use TheFramework\Helpers\HelperJson;
 
 final class LoginController extends RestrictController
 {
-    private LoginService $login;
-
     public function index(): void
     {
         $this->add_var("pagetitle", "LOGIN");
@@ -24,27 +22,21 @@ final class LoginController extends RestrictController
 
     public function access(): void
     {
-        //sleep(15);
-        //$this->sess_add("user", $this->get_post("email"))->add("pass",$this->get_post("password"));
-        //$this->sess_destroy();
-
-        $this->login = SF::get("Restrict\Login", $this->get_post());
+        $login = SF::get("Restrict\Login", $this->get_post());
         $this->logd("middle start");
         $oJson = new HelperJson();
         try{
-            //$oServ = new LoginMiddleService($this->get_post());
-            //$token = $oServ->get_token();
-            $this->login->access();
-            $oJson->set_payload(["token"=>$this->sess_get()])->show();
+            $login->access();
+            $oJson->set_payload(["message"=>__("auth ok")])->show();
         }
         catch (\Exception $e)
         {
-            $this->logerr($e->getMessage(),"LoginController.middle");
+            $this->logerr($e->getMessage(),"LoginController.access");
             $oJson->set_code(HelperJson::CODE_UNAUTHORIZED)
                 ->set_error([$e->getMessage()])
                 ->show(1);
         }
-        $this->logd("middle end");
+        $this->logd("access end");
     }
 
 }//LoginController
