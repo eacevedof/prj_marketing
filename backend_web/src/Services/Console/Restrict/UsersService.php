fcf5974537ed0a82f0fe66b90f55153177777f05
ioedumac <eacevedof@yahoo.es>
2021-11-01 19:30:04 +0100

    fix component

--- backend_web/src/Services/Console/Restrict/UsersService.php
+++ backend_web/src/Services/Console/Restrict/UsersService.php
@@ -11,28 +11,30 @@ namespace App\Services\Console\Restrict;
 use App\Services\AppService;
 use App\Services\Console\IConsole;
 use TheFramework\Components\Session\ComponentEncdecrypt;
+use App\Traits\ConsoleTrait;
 
 final class UsersService extends AppService implements IConsole
 {
-    private array $input;
+    use ConsoleTrait;
+
     private ComponentEncdecrypt $encdec;
+    private string $word;
 
     public function __construct(array $input)
     {
-        $this->input = $input;
+        $this->word = $input[0] ?? ":)";
         $this->encdec = $this->_get_encdec();
     }
 
     private function _get_password(): string
     {
-        $word = $this->input[0] ?? ":)";
-        return $this->encdec->get_hashpassword($word);
+        return $this->encdec->get_hashpassword($this->word);
     }
 
     //php run.php users 1234
     public function run(): void
     {
-        echo $this->input[0];
+        $this->_pr($this->word);
         $password = $this->_get_password();
         $message = "password: {$password}";
         $this->logpr($message);
