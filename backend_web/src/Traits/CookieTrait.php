f2a274b449b1eb71eb06324fe174f5e2dbaec855
ioedumac <eacevedof@yahoo.es>
2021-11-02 21:59:03 +0100

    session component cookie

---
+++ backend_web/src/Traits/CookieTrait.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link eduardoaf.com
+ * @name App\Traits\SessionTrait
+ * @file SessionTrait.php 1.0.0
+ * @date 21-07-2020 19:00 SPAIN
+ * @observations
+ */
+namespace App\Traits;
+
+use TheFramework\Components\Session\ComponentCookie;
+
+trait CookieTrait
+{
+    private ?ComponentCookie $cookie = null;
+
+    protected function _cookieinit(): SessionComponent
+    {
+        if(!$this->cookie) $this->cookie = new ComponentCookie();
+        return $this->session;
+    }
+
+}//SessionTrait
