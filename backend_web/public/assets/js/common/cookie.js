5d65b5773b69c5b56185029d7f4fca6b6770ceb8
ioedumac <eacevedof@yahoo.es>
2021-11-03 21:25:27 +0100

    cookie

--- backend_web/public/assets/js/common/cookie.js
+++ backend_web/public/assets/js/common/cookie.js
@@ -1,26 +1,20 @@
-export const get_cookie = cookiename  => {
-  const nameEQ = cookiename + "="
-  const chars = document.cookie.split("")
+export const get_cookie = (name, value, days) => {
 
-  for(let i=0; i < chars.length; i++) {
-    let c = chars[i]
-    while (c.charAt(0)===" ")
-      c = c.substring(1,c.length)
+}
 
-    if (c.indexOf(nameEQ) === 0)
-      return c.substring(nameEQ.length, c.length)
+const set_cookie = (name, value, days) => {
+  let strexpire = ""
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+    strexpire = "; expires=" + date.toUTCString()
   }
-  return null
+
+  document.cookie = name.concat("=")
+    .concat(value || "").concat(strexpire).concat("; path=/")
 }
 
-const set_cookie = (cookiename, value, expiredays) => {
-  const expdate = new Date()
-  expdate.setDate(expdate.getDate() + expiredays)
-  const cookieval = escape(value) + ((expiredays === null) ? "" : "; expires=" + expdate.toUTCString())
-  document.cookie = cookiename + "=" + cookieval
-}
-
-export const erase = cookiename => document.cookie = cookiename + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+export const erase_cookie = name => document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
 
 export default set_cookie
 
