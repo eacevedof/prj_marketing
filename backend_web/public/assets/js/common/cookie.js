0306ed13d58a9fd1342b07e8a34c5a6245f9f6ac
ioedumac <eacevedof@yahoo.es>
2021-11-03 22:06:41 +0100

    cookie

--- backend_web/public/assets/js/common/cookie.js
+++ backend_web/public/assets/js/common/cookie.js
@@ -1,18 +1,11 @@
 export const get_cookie = name => {
-  const nameEQ = name.concat("=")
-  const parts = document.cookie.split(";")
-
-  //lang=es; MARKETINGID=c97b6ee7e06bc76fc75763ba9745bd3b; login=ok'
-  for(let i=0; i < parts.length; i++) {
-    let c = parts[i];
-    while (c.charAt(0) === " ")
-      c = c.substring(1, c.length)
-
-    if (c.indexOf(nameEQ) === 0)
-        return c.substring(nameEQ.length,c.length);
-  }
-
-  return null
+  const parts = document.cookie.split("; ")
+  const obj = parts
+                .map(str => str.split("="))
+                .map(ar => ({key:ar[0], value:ar[1]}))
+                .filter(obj => obj.key === name)
+                .map(obj => obj.value)
+  return obj.length ? obj[0] : null
 }
 
 const set_cookie = (name, value, days) => {
@@ -30,11 +23,3 @@ const set_cookie = (name, value, days) => {
 export const erase_cookie = name => document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
 
 export default set_cookie
-
-/*
-rodrigo carla jorge
-natalia maria xavi
-eduardo mayte
-undefined
-
- */
