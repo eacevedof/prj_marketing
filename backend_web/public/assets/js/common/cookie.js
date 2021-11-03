8ae62597e5e41c8f528e86f204178f9f84c0e6cd
ioedumac <eacevedof@yahoo.es>
2021-11-03 20:50:23 +0100

    cookie

--- backend_web/public/assets/js/common/cookie.js
+++ backend_web/public/assets/js/common/cookie.js
@@ -1,12 +1,20 @@
 export const get_cookie = cookiename  => {
-  let name = cookiename.concat("=")
+  const nameEQ = cookiename + "="
+  var ca = document.cookie.split("")
+
+  for(var i=0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0)==" ") c = c.substring(1,c.length)
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length)
+  }
+  return null
 }
 
 const set_cookie = (cookiename, value, expiredays) => {
-  const expdate = new Date();
-  expdate.setDate(expdate.getDate() + expiredays);
-  const cookieval = escape(value) + ((expiredays === null) ? "" : "; expires=" + expdate.toUTCString());
-  document.cookie = cookiename + "=" + cookieval;
+  const expdate = new Date()
+  expdate.setDate(expdate.getDate() + expiredays)
+  const cookieval = escape(value) + ((expiredays === null) ? "" : "; expires=" + expdate.toUTCString())
+  document.cookie = cookiename + "=" + cookieval
 }
 
 export const erase = cookiename => document.cookie = cookiename + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
