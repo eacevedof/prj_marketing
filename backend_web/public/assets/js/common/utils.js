269c3d7bd9df6f9d2e8d04cc3fe9d9fd42a8fc99
ioedumac <eacevedof@yahoo.es>
2021-11-07 19:45:32 +0100

    utils.js

--- backend_web/public/assets/js/common/utils.js
+++ backend_web/public/assets/js/common/utils.js
@@ -11,3 +11,11 @@ export const onDocumentready = callbackFunction =>{
   else
     document.addEventListener("DOMContentLoaded", callbackFunction)
 }
+
+export const debounce = (func, timeout = 300) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
