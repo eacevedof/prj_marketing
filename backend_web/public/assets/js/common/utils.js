8f02eaca71c6d170c25df4a8b518cd5f8243476d
ioedumac <eacevedof@yahoo.es>
2021-11-09 19:44:01 +0100

    url.js

--- backend_web/public/assets/js/common/utils.js
+++ backend_web/public/assets/js/common/utils.js
@@ -1,16 +1,3 @@
-export const getUrlParameter = (name) => {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  const regex = new RegExp(`[\\?&]${name}=([^&#]*)`);
-  const results = regex.exec(location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-export const getUrlPosition = pos => {
-  let parts = location.pathname.split("?")
-  parts = parts[0].split("/")
-  return parts[pos] ?? ""
-}
-
 export const onDocumentready = callbackFunction =>{
   if(document.readyState != 'loading')
     callbackFunction(event)
@@ -24,28 +11,4 @@ export const debounce = (func, timeout = 300) => {
     clearTimeout(timer);
     timer = setTimeout(() => { func.apply(this, args); }, timeout);
   };
-}
-
-export const addPageInUrlByPosition = (pos) => {
-  let page = getUrlPosition(pos)
-  let url = location.pathname
-  if (isNaN(page) || parseInt(page)<1) {
-    page = 1
-    url = url.concat(`/${page}`)
-    window.history.pushState({}, "", url)
-  }
-}
-
-export const addPageToUrl = (page, pos) => {
-  let url = location.pathname
-  let parts1 = url.split("?")
-  let parts2 = parts1[0].split("/")
-  parts2[pos] = page
-  const p = [
-    parts2.join("/"),
-    "?",
-    parts1[1] ?? ""
-  ]
-  url  = p.join("")
-  window.history.pushState({}, "", url)
 }
