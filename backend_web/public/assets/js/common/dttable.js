8dec5fa0e4b215c3f9f7c1ed9bc55a32418bb10d
ioedumac <eacevedof@yahoo.es>
2021-11-14 12:05:30 +0100

    dttable.js

--- backend_web/public/assets/js/common/dttable.js
+++ backend_web/public/assets/js/common/dttable.js
@@ -1,1 +1,24 @@
 //alert("hola")
+import {debounce} from "./utils.js"
+import {
+  add_page_to_url, get_page_from_url
+} from "./url.js"
+
+let is_rendered = false
+let $dttable = null
+
+const dt_render = (options) => {
+  let tableid = options.table_id
+  const tablesel = `#${tableid}`
+  const $table = document.getElementById(tableid)
+
+  $dttable.on("page.dt", function() {
+    const pagemin = $dttable.page.info()?.page ?? 0
+    add_page_to_url(pagemin+1, 3)
+  })
+  .on("order.dt", function() {
+    if (is_rendered) add_page_to_url(1, 3)
+  })
+}
+
+export default dt_render
