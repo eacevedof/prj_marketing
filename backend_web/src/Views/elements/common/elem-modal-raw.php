97634db534e9d4a5de84361e03e4b0c668710dab
ioedumac <eacevedof@yahoo.es>
2021-11-16 23:25:56 +0100

    elem-modal-raw

---
+++ backend_web/src/Views/elements/common/elem-modal-raw.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @var \App\Views\AppView $this
+*/
+?>
+<div id="modal-generic" class="modal-wrapper">
+    <div class="modal-dialog modal-dialog-grid" role="modal-dialog">
+        <header class="area-header">
+            <h2 role="title"></h2>
+            <button type="button" role="btn-close">x</button>
+        </header>
+        <div class="area-body" role="body">
+        </div>
+    </div>
+</div>
+<?=$this->_asset_js_module("common/modal-raw")?>
+
