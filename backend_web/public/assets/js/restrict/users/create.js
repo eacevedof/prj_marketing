0ce2a72b89a7ab14dcf5319b8f351b3d7eb5ce5b
ioedumac <eacevedof@yahoo.es>
2021-11-17 20:28:54 +0100

    carga dinamica parece q va

---
+++ backend_web/public/assets/js/restrict/users/create.js
@@ -0,0 +1,77 @@
+import set_cookie from "/assets/js/common/cookie.js"
+const ID_WRAPPER = "#app"
+const URL = "/login/access"
+
+const App = {
+  data() {
+    return {
+      email: "eaf@eaf.com",
+      password: "1234",
+      issending: false,
+      btnsend: "enviar"
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      this.issending = true
+      this.btnsend = "Enviando..."
+
+      fetch(URL, {
+        method: "post",
+        headers: {
+          "Accept": "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          csrf: document.getElementById("_csrf")?.value ?? "",
+          email: this.email,
+          password: this.password,
+        })
+      })
+        .then(response => response.json())
+        .then(response => {
+          this.issending = false
+          this.btnsend = "Enviar"
+
+          if(response?.errors?.length){
+            console.error(response.errors)
+            return Swal.fire({
+              icon: "warning",
+              title: "Proceso incompleto",
+              html: "No se ha podido procesar tu mensaje. Por favor inténtalo más tarde. Disculpa las molestias. <br/>"+response.errors[0],
+            })
+          }
+
+          console.log("reponse ok",response)
+          set_cookie("lang", response.data.lang)
+          window.location = "/restrict"
+
+          Swal.fire({
+            icon: "success",
+            title: "Gracias por contactar conmigo!",
+            html: "En breves momentos recibirás una copia del mensaje en tu email.",
+          })
+
+        })
+        .catch(error => {
+          console.error("catch.error", error)
+          Swal.fire({
+            icon: "error",
+            title: "Vaya! Algo ha ido mal (c)",
+            html: "No se ha podido procesar tu mensaje. Por favor inténtalo más tarde. Disculpa las molestias.",
+          })
+        })
+        .finally(()=>{
+          this.issending = false
+          this.btnsend = "Enviar"
+        })
+
+    }//onSubmit
+
+  }//methods
+}
+
+Vue.createApp(App).mount(ID_WRAPPER)
+
+export default () => Vue.createApp(App).mount(ID_WRAPPER)
--- backend_web/src/Views/templates/restrict/users/create.tpl
+++ backend_web/src/Views/templates/restrict/users/create.tpl
@@ -4,11 +4,31 @@
  */
 ?>
 <h1><?=$h1?></h1>
-<form>
-    <label>user</label>
-    <input type="text" id="txt-user">
-    <button type="button" onclick="alert('envio')">boton de prueba</button>
-</form>
-<script type="module">
-document.getElementById("txt-user").focus()
-</script>
+<div id="app">
+    <form @submit.prevent="onSubmit">
+        <input type="hidden" id="_csrf" value="<?=$csrf??""?>" />
+        <div>
+            <label for="email"><?=__("Email")?> *</label>
+            <input type="email" id="email" v-model="email" placeholder="<?=__("your email")?>" required="required">
+        </div>
+        <div>
+            <label for="password"><?=__("Password")?> *</label>
+            <input id="password" type="password" v-model="password" placeholder="<?=__("your secret password")?>" required="required">
+        </div>
+        <div>
+            <label htmlFor="text"><?=__("Full name")?> *</label>
+            <input type="text" v-model="text" required="required">
+        </div>
+        <div>
+            <label htmlFor="text"><?=__("Full name")?> *</label>
+            <input type="text" v-model="text" required="required">
+        </div>
+        <div>
+            <button id="btn-contact" class="btn btn-dark" :disabled="issending" >
+                {{btnsend}}
+                <img v-if="issending" src="/assets/images/common/loading.png" width="25" height="25"/>
+            </button>
+        </div>
+    </form>
+</div>
+<script type="module" src="/assets/js/restrict/users/create.js"></script>
