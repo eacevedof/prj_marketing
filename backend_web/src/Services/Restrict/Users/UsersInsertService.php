0bd665456521afa05a0ddd0f0a5c8aa73d7edd41
ioedumac <eacevedof@yahoo.es>
2021-11-19 17:59:31 +0100

    fix get_crud

--- backend_web/src/Services/Restrict/Users/UsersInsertService.php
+++ backend_web/src/Services/Restrict/Users/UsersInsertService.php
@@ -7,12 +7,14 @@ use App\Services\AppService;
 use App\Repositories\Base\UserRepository;
 use App\Traits\SessionTrait;
 use App\Enums\Key;
+use TheFramework\Components\Session\ComponentEncdecrypt;
 
 
 final class UsersInsertService extends AppService
 {
     use SessionTrait;
     private array $input;
+    private ComponentEncdecrypt $encdec;
     private UserRepository $repository;
 
     public function __construct(array $input)
@@ -20,6 +22,7 @@ final class UsersInsertService extends AppService
         $this->repository = RepositoryFactory::get("Base/UserRepository");
         $this->input = $input;
         $this->_sessioninit();
+        $this->encdec = $this->_get_encdec();
     }
 
     public function __invoke(): string
@@ -27,7 +30,7 @@ final class UsersInsertService extends AppService
         $insert = $this->input;
         $f = array_filter(array_keys($insert), function ($k){ return substr($k,0,1)!=="_"; });
         $insert = array_intersect_key($insert, array_flip($f));
-        $insert["secret"] = $insert["password"];
+        $insert["secret"] = $this->encdec->get_hashpassword($insert["password"]);
         unset($insert["password"]);
         $insert["insert_user"] = $this->session->get(Key::AUTH_USER)["id"] ?? "";
         $insert["insert_date"] = date("Y-m-d H:i:s");
