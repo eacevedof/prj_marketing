75fc902adcad10cad1d96c5955bf65038dfe0668
ioedumac <eacevedof@yahoo.es>
2021-11-20 19:06:49 +0100

    fix busqueda

---
+++ backend_web/public/assets/js/common/fielderrors.js
@@ -0,0 +1,65 @@
+const tpl = `
+    <div approle="field-error" class="%css%">
+      <ul>%lis%</ul>
+    </div>
+  `
+let fieldsid = []
+let $wrapper = null
+let errors = []
+
+const clear_errors = () => {
+  const errors = Array.from($wrapper.querySelectorAll(`[approle="field-error"]`))
+  errors.forEach($div => $div.parentNode.removeChild($div))
+}
+
+export const field_errors = () => {
+  clear_errors()
+  const nonfieldsid = errors
+                        .filter(objerr => !fieldsid.includes(objerr.field))
+                        .map(objerr => objerr.field)
+
+  const fielderrors = []
+  fieldsid.forEach(id => {
+    fielderrors.push({
+      id,
+      messages : errors.filter(objerr => objerr.field === id).map(objerr => objerr.message)
+    })
+  })
+
+  fielderrors.forEach(objfield => {
+    const lis = objfield.messages.map(message => `<li>${message}</li>`).join("")
+    const html = tpl.replace("%lis%",lis).replace("%css%","")
+    let $input = $wrapper.querySelector(`#${objfield.id}`)
+    if ($input) {
+      $input.insertAdjacentHTML("afterend", html)
+      $input.classList.add("form-error")
+      $input.focus()
+    }
+  })
+
+  const nonerrors = []
+  nonfieldsid.forEach(id => {
+    nonerrors.push({
+      id,
+      label: errors.filter(objerr => objerr.field === id).map(objerr => objerr.label).join(""),
+      messages : errors.filter(objerr => objerr.field === id).map(objerr => objerr.message)
+    })
+  })
+
+  nonerrors.forEach(objfield => {
+    const lis = objfield.messages.map(message => `<li>${message}</li>`).join("")
+    const html = tpl
+      .replace("%lis%",`<li class="li-label">${objfield.label}</li>${lis}`)
+      .replace("%css%"," error-top")
+    $wrapper.insertAdjacentHTML("afterbegin", html)
+  })
+}
+
+const set_config = options => {
+  fieldsid = options.fields
+  $wrapper = options.wrapper
+  errors = options.errors
+  console.log(options)
+}
+
+export default set_config
