4b99aec8f0e72393cb846c4eb03ee117923eb83e
ioedumac <eacevedof@yahoo.es>
2021-11-20 19:06:49 +0100

    fix busqueda

--- backend_web/public/assets/js/restrict/users/create.js
+++ backend_web/public/assets/js/restrict/users/create.js
@@ -1,3 +1,5 @@
+import set_config, {field_errors} from "/assets/js/common/fielderrors.js"
+
 const ID_WRAPPER = "#vue-users-create"
 const URL_POST = "/restrict/users/insert"
 const URL_REDIRECT = "/restrict/users"
@@ -12,66 +14,16 @@ const fields = {
   fullname: "",
   address: "",
   birthdate: "",
-
-  issending: false,
-  btnsend: "enviar",
   phone: "x",
-}
-
-function clear_errors(){
-  const errors = Array.from($wrapper.querySelectorAll(`[approle="field-error"]`))
-  errors.forEach($div => $div.parentNode.removeChild($div))
-}
-
-function fields_errors(errors) {
-  const fieldsid = Object.keys(fields)
-  const nonfieldsid = errors.filter(objerr => !fieldsid.includes(objerr.field)).map(objerr => objerr.field)
-  
-  const fielderrors = []
-  fieldsid.forEach(id => {
-    fielderrors.push({
-      id,
-      messages : errors.filter(objerr => objerr.field === id).map(objerr => objerr.message)
-    })
-  })
-
-  const tpl = `
-    <div approle="field-error" class="%css%">
-      <ul>%lis%</ul>
-    </div>
-  `
-  fielderrors.forEach(objfield => {
-    const lis = objfield.messages.map(message => `<li>${message}</li>`).join("")
-    const html = tpl.replace("%lis%",lis).replace("%css%","")
-    let $input = $wrapper.querySelector(`#${objfield.id}`)
-    if ($input) {
-      $input.insertAdjacentHTML("afterend", html)
-      $input.classList.add("form-error")
-      $input.focus()
-    }
-  })
-
-  const nonerrors = []
-  nonfieldsid.forEach(id => {
-    nonerrors.push({
-      id,
-      label: errors.filter(objerr => objerr.field === id).map(objerr => objerr.label).join(""),
-      messages : errors.filter(objerr => objerr.field === id).map(objerr => objerr.message)
-    })
-  })
-  console.log(nonerrors)
-  nonerrors.forEach(objfield => {
-    const lis = objfield.messages.map(message => `<li>${message}</li>`).join("")
-    const html = tpl
-                  .replace("%lis%",`<li class="li-label">${objfield.label}</li>${lis}`)
-                  .replace("%css%"," error-top")
-    $wrapper.insertAdjacentHTML("afterbegin", html)
-  })
 }
 
 const App = {
   data() {
-    return fields
+    return {
+      ...fields,
+      issending: false,
+      btnsend: "enviar",
+    }
   },
 
   methods: {
@@ -100,16 +52,19 @@ const App = {
       .then(response => response.json())
       .then(response => {
         console.log("response",response)
-        clear_errors()
         this.issending = false
         this.btnsend = "Enviar"
 
         if(response?.errors?.length){
           const errors = response.errors[0]?.fields_validation
-          if(errors){
-            return fields_errors(errors)
+          if(errors) {
+            set_config({
+              fields: Object.keys(fields),
+              wrapper: $wrapper,
+              errors
+            })
+            return field_errors(errors)
           }
-          //vendria errors[0].fields_validation
           return Swal.fire({
             icon: "warning",
             title: "Proceso incompleto",
@@ -119,7 +74,6 @@ const App = {
         window.location = URL_REDIRECT
       })
       .catch(error => {
-        console.error("catch.error", error)
         Swal.fire({
           icon: "error",
           title: "Vaya! Algo ha ido mal (c)",
@@ -134,7 +88,8 @@ const App = {
     }//onSubmit
 
   }//methods
-}
+
+}// App
 
 //si esto se ejecuta desde aqui solo se ve bien en la primera llamada
 // en las siguientes no carga el form con vue. Por eso es mejor usar export
