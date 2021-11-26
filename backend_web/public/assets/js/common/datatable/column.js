3e2b12bff15332c1c4c11930319eb01ee83bec71
ioedumac <eacevedof@yahoo.es>
2021-11-26 17:33:08 +0100

    column fix

--- backend_web/public/assets/js/common/datatable/column.js
+++ backend_web/public/assets/js/common/datatable/column.js
@@ -1,6 +1,6 @@
 let ths = []
 
-const get_colums = () => ths
+const _get_columns = () => ths
   .map($th => $th.getAttribute("column"))
   .filter(col => col!=="")
 
@@ -20,13 +20,55 @@ const get_type = column => ths.filter(
   $th => $th.getAttribute("column") === column
 ).map($th => $th.getAttribute("type"))
 
+const get_columns = () => {
+  const cols = _get_columns()
+  //col con numero
+  const allcols = [{
+    searchable: false,
+    orderable: false,
+    targets: 0,
+    data: null,
+  }]
+
+  cols.forEach((colname, i )=> {
+    const obj = {
+      targets: i+1,
+      data: colname,
+      //searchable: false, no afecta en nada
+      visible: is_visible(colname),
+      render: function (row) {
+        return row
+      }
+    }
+
+    allcols.push(obj)
+  })
+
+  allcols.push({
+    targets: -1,
+    data: null,
+    render: function(row) {
+      //type: display
+      const uuid = row.uuid ?? ""
+      if(!uuid) return ""
+      const links = [
+        `<button type="button" uuid="${uuid}" approle="rowbtn-show">show</button>`,
+        `<button type="button" uuid="${uuid}" approle="rowbtn-edit">edit</button>`,
+        `<button type="button" uuid="${uuid}" approle="rowbtn-del">del</button>`,
+      ]
+      return links.join("&nbsp;");
+    },
+  })
+
+  return allcols
+}
+
 export default ($table) => {
   if(ths.length === 0)
     ths = Array.from($table.querySelectorAll(`[column]`))
 
   return {
-    get_colums,
-    is_visible,
+    get_columns,
     is_ordenable,
     is_searchable,
     get_type,
