984d05e5978abb146dd9aa77d4f39f37e716a1f9
ioedumac <eacevedof@yahoo.es>
2021-11-28 14:05:27 +0100

    configurando lit

--- backend_web/public/assets/js/vendor/lit.dev/lit.js
+++ backend_web/public/assets/js/vendor/lit.dev/lit.js
@@ -1,4 +1,4 @@
-import"./reactive-element.js";
-import"/-/lit-html@v2.0.0-rc.3-U0mgczQMV615aaRpIg4r/dist=es2020,mode=imports,min/optimized/lit-html.js";
+import"./reactive-element/reactive-element.js";
+import"./lit-html.js";
 export*from"/-/lit-element@v3.0.0-rc.2-8MhiXIS5YKCp1A85OqeG/dist=es2020,mode=imports,min/optimized/lit-element/lit-element.js";
 export default null;
