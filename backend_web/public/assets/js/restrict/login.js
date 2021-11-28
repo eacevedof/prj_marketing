948a31446b588eff50cb6d4d4238c1c879c33add
ioedumac <eacevedof@yahoo.es>
2021-11-28 20:47:31 +0100

    login.js con lit-element

--- backend_web/public/assets/js/restrict/login.js
+++ backend_web/public/assets/js/restrict/login.js
@@ -1,35 +1,44 @@
-import set_cookie from "../common/cookie.js"
+import {html, LitElement} from "/assets/js/vendor/lit.dev/lit-bundle.js"
+import set_cookie from "/assets/js/common/cookie.js"
 
 const URL = "/login/access"
 const URL_ON_ACCESS = "/restrict/users"
 
-const App = {
-  data() {
-    return {
-      email: "",
-      password: " ",
-      issending: false,
-      btnsend: "Enviar"
-    }
-  },
+export class FormLogin extends LitElement {
 
-  methods: {
-    onSubmit() {
-      this.issending = true
-      this.btnsend = "Enviando..."
+  static properties = {
+    csrf: {type: String},
+    issending: {type: Boolean},
+    btnsend: {type: String},
+  }
 
-      fetch(URL, {
-        method: "post",
-        headers: {
-          "Accept": "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          _csrf: document.getElementById("_csrf")?.value ?? "",
-          email: this.email,
-          password: this.password,
-        })
+  constructor() {
+    super()
+    this.email = ""
+    this.password = ""
+    this.issending = false
+    this.btnsend = "Enviar"
+  }
+
+  $get = sel => this.shadowRoot.querySelector(sel)
+
+  submitForm(e) {
+    e.preventDefault();
+    this.issending = true
+    this.btnsend = "...enviando"
+
+    fetch(URL, {
+      method: "post",
+      headers: {
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        _csrf: this.csrf,
+        email: this.$get("#email").value,
+        password: this.$get("#password").value,
       })
+    })
       .then(response => response.json())
       .then(response => {
         this.issending = false
@@ -55,7 +64,6 @@ const App = {
         })
 
         setTimeout(() => window.location = URL_ON_ACCESS, 1000)
-
       })
       .catch(error => {
         Swal.fire({
@@ -68,10 +76,30 @@ const App = {
         this.issending = false
         this.btnsend = "Enviar"
       })
+  }//submit
 
-    }//onSubmit
+  render() {
+    return html`
+    <form @submit=${this.submitForm}>
+      <div class="form-controls">
+        <div>
+          <label for="email">Email</label>
+          <input type="email" id="email" .value=${this.email} />
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input type="password" id="password" .value=${this.password} />
+        </div>
+        <button type="submit" ?disabled=${this.issending}>
+          ${this.btnsend}
+          ${this.issending ? html`<img src="/assets/images/common/loading.png" width="25" height="25"/>`: html``}
+        </button>
+      </div>
+    </form>
+    `;
+  }
 
-  }//methods
-}
+}//FormLogin
 
-Vue.createApp(App).mount("#app")
+customElements.define("form-login", FormLogin);
+
