8087b9725c6c29bcc35f672c83c8f14c561ab226
ioedumac <eacevedof@yahoo.es>
2021-11-28 14:07:28 +0100

    lit.js ok

--- backend_web/public/assets/js/vendor/lit.dev/lit.js
+++ backend_web/public/assets/js/vendor/lit.dev/lit.js
@@ -1,4 +1,5 @@
+//ok
 import"./reactive-element/reactive-element.js";
 import"./lit-html.js";
-export*from"/-/lit-element@v3.0.0-rc.2-8MhiXIS5YKCp1A85OqeG/dist=es2020,mode=imports,min/optimized/lit-element/lit-element.js";
+export*from"./lit-element/lit-element.js";
 export default null;
