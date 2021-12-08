d380bd2a2554f326ee5172d8191d5e97eb756cb0
ioedumac <eacevedof@yahoo.es>
2021-12-08 21:10:22 +0100

    replica validador en update

--- backend_web/src/Services/Restrict/Users/UsersUpdateService.php
+++ backend_web/src/Services/Restrict/Users/UsersUpdateService.php
@@ -71,6 +71,20 @@ final class UsersUpdateService extends AppService
             ->add_rule("birthdate", "empty", function ($data) {
                 return trim($data["value"]) ? false : __("Empty field is not allowed");
             })
+            ->add_rule("id_profile", "empty", function ($data){
+                return trim($data["value"]) ? false : __("Empty field is not allowed");
+            })
+            ->add_rule("id_parent", "by-profile", function ($data){
+                if (($data["data"]["id_profile"] ?? "") === "4" && !trim($data["value"]))
+                    return __("Empty field is not allowed");
+                return false;
+            })
+            ->add_rule("id_country", "empty", function ($data){
+                return trim($data["value"]) ? false : __("Empty field is not allowed");
+            })
+            ->add_rule("id_language", "empty", function ($data){
+                return trim($data["value"]) ? false : __("Empty field is not allowed");
+            })
             ->add_rule("password", "not-equal", function ($data){
                 if(!($password = trim($data["value"]))) return false;
                 $password2 = trim($data["data"]["password2"] ?? "");
