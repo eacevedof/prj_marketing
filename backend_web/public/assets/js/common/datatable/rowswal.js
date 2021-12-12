69e4b861ad24470befe421d2f8a7e3362370d302
ioedumac <eacevedof@yahoo.es>
2021-12-12 16:20:07 +0100

    undelete front ok

--- backend_web/public/assets/js/common/datatable/rowswal.js
+++ backend_web/public/assets/js/common/datatable/rowswal.js
@@ -11,8 +11,14 @@ let _texts = {
     title: "Data successfully deleted",
   },
   delswal: {
-    title: "Are you sure?",
+    title: "Delete operation",
     text: "You will not be able to recover this information! <b>",
+    confirm: "Yes, I am sure!",
+    cancel: "No, cancel it!",
+  },
+  undelswal: {
+    title: "Restore operation",
+    text: "",
     confirm: "Yes, I am sure!",
     cancel: "No, cancel it!",
   }
@@ -76,11 +82,38 @@ const on_delete = uuid =>
       .catch(error => _show_error_catched(error))
   })//end then
 
+const on_undelete = uuid =>
+  Rowswal.fire({
+    title: _texts.undelswal.title,
+    html: _texts.undelswal.text.concat(uuid).concat("</b>"),
+    confirmButtonText: _texts.undelswal.confirm,
+    cancelButtonText: _texts.undelswal.cancel,
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#DD6B55",
+    closeOnConfirm: false,
+    closeOnCancel: false
+  })
+  .then(result => {
+    if (!result.isConfirmed) return
+    const url = _$table.getAttribute("urlmodule").concat(`/undelete/${uuid}`)
+    fetch(url,{method:"patch"})
+      .then(response => response.json())
+      .then(json => {
+        if (json.errors.length>0)
+          return _show_error_handled()
+        _show_success(uuid)
+        _dttable.ajax.reload()
+      })
+      .catch(error => _show_error_catched(error))
+  })//end then
+
 export default ($table, dttable) => {
   _$table = $table
   _dttable = dttable
 
   return {
-    on_delete
+    on_delete,
+    on_undelete
   }
 }
