8658f6672caffcd394bfe94f00670ed3ac154f1b
ioedumac <eacevedof@yahoo.es>
2021-12-13 20:08:53 +0100

    authcomponent paso de this->user a self::user

--- backend_web/src/Components/Auth/AuthComponent.php
+++ backend_web/src/Components/Auth/AuthComponent.php
@@ -9,50 +9,46 @@ use App\Enums\KeyType;
 final class AuthComponent
 {
     private SessionComponent $session;
-    private ?array $user;
+    private static ?array $user = null;
     
     public function __construct()
     {
         $this->session = SF::get();
-        $this->user = $this->session->get(KeyType::AUTH_USER);
+        if (!self::$user)
+            self::$user = $this->session->get(KeyType::AUTH_USER);
     }
 
     public function get_user(): ?array
     {
-        return $this->user;
-    }
-
-    public function is_user_logged(): bool
-    {
-        return (bool)($this->user["id"] ?? "");
+        return self::$user;
     }
 
     public function is_user_allowed(string $action): bool
     {
-        if(!$this->user) return false;
+        if(!self::$user) return false;
         if($this->is_root()) return true;
         
-        $permissions = SF::get()->get(KeyType::AUTH_USER_PERMISSIONS);
+        $permissions = $this->session->get(KeyType::AUTH_USER_PERMISSIONS);
         return in_array($action, $permissions);
     }
 
     public function is_root(): bool
     {
-        return (($this->user["id_profile"] ?? "") === ProfileType::ROOT);
+        return ((self::$user["id_profile"] ?? "") === ProfileType::ROOT);
     }
 
     public function is_sysadmin(): bool
     {
-        return (($this->user["id_profile"] ?? "") === ProfileType::SYS_ADMIN);
+        return ((self::$user["id_profile"] ?? "") === ProfileType::SYS_ADMIN);
     }
 
     public function is_business_owner(): bool
     {
-        return (($this->user["id_profile"] ?? "") === ProfileType::BUSINESS_OWNER);
+        return ((self::$user["id_profile"] ?? "") === ProfileType::BUSINESS_OWNER);
     }
 
     public function is_business_manager(): bool
     {
-        return (($this->user["id_profile"] ?? "") === ProfileType::BUSINESS_MANAGER);
+        return ((self::$user["id_profile"] ?? "") === ProfileType::BUSINESS_MANAGER);
     }
 }
