13c2ad4bb86a2f5c05cebe367d8d269d244e4300
ioedumac <eacevedof@yahoo.es>
2022-01-15 16:57:32 +0100

    cache handler

---
+++ backend_web/src/Components/DiskCache/HierarchyComponent.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Components\DiskCache;
+
+final class DiskCacheComponent
+{
+    private string $pathcache = PATH_DISK_CACHE ?? "./";
+    private string $pathsub = "";
+    protected int $time = 3600;
+    private string $keyname = "";
+    private string $hashname = "";
+    private string $pathfinal = "";
+
+    private function _load_hashname(): self
+    {
+        $this->hashname = md5($this->keyname);
+        return $this;
+    }
+
+    private function _load_pathfinal(): self
+    {
+        $path = $this->pathcache;
+        if ($this->pathsub) $path .= "/$this->pathsub";
+        $this->pathfinal = $path;
+    }
+
+
+    private function _get_cached_file(): string
+    {
+        $files = scandir($this->pathfinal);
+        if (count($files) == 2) return [];
+        
+        $files = array_filter($files, function ($file) {
+           return strstr($file, $this->hashname); 
+        });
+        return $files[0] ?? "";
+    }
+
+    private function _get_dietime(string $date): int
+    {
+        //$now = date("Y-m-d H:i:s");
+        return (int) date("YmdHis", (strtotime($date) + $this->time));
+    }
+
+    private function _is_alive(string $filename): bool
+    {
+        if (!$filename) return false;
+        $end = explode("-",$filename);
+        $end = end($end);
+        if (!$end || !is_numeric($end)) return false;
+        $dietime = $this->_get_dietime($end);
+        return ($dietime>(int) date("YmdHis"));
+    }
+
+    public function write(string $content): self
+    {
+        $this->_load_hashname()->_load_pathfinal();
+        $file = $this->_get_cached_file();
+        if ($this->_is_alive($file)) return $this;
+
+
+    }
+
+    public function read(string $content): self
+    {
+
+        return $this;
+    }
+
+    public function set_folder(string $pathsub): self
+    {
+        $this->pathsub = $pathsub;
+        return $this;
+    }
+    
+    public function set_keyname(string $keyname): self
+    {
+        $this->keyname = $keyname;
+        return $this;
+    }
+
+    public function set_time(int $time): self
+    {
+        $this->time = $time;
+        return $this;
+    }
+
+}
