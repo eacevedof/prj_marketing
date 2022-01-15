6675536c0ef0e2a839c18c25c9342ca25414c216
ioedumac <eacevedof@yahoo.es>
2022-01-15 16:57:32 +0100

    cache handler

---
+++ backend_web/src/Traits/DiskCacheTrait.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link eduardoaf.com
+ * @name App\Traits\DiskCacheTrait
+ * @file DiskCacheTrait.php 1.0.0
+ * @date 21-07-2020 19:00 SPAIN
+ * @observations
+ */
+namespace App\Traits;
+
+trait DiskCacheTrait
+{
+
+
+    protected function save_into_disk(string $content, string $key): void
+    {
+
+    }
+
+}//DiskCacheTrait
