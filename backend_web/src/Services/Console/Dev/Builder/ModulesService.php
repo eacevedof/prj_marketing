5c375395efb92f0055882ee16a313e7dd0193c9b
ioedumac <eacevedof@yahoo.es>
2022-01-16 15:51:32 +0100

    crea carpeta destino

--- backend_web/src/Services/Console/Dev/Builder/ModulesService.php
+++ backend_web/src/Services/Console/Dev/Builder/ModulesService.php
@@ -19,12 +19,16 @@ final class ModulesService extends AppService implements IConsole
 {
     use ConsoleTrait;
     private SchemaService $schema;
+    private const PATH_XXXMODULE = PATH_SRC."/xxx-module";
+    private const PATH_FILESTPL = PATH_SRC."/xxx-module/files";
+    private string $pathbuild = "";
 
     public function __construct(array $input)
     {
         $this->input = $input[0] ?? "";
         $this->schema = new SchemaService(DF::get_by_default());
         $this->_check_input();
+        $this->pathbuild = self::PATH_XXXMODULE . "/module-".date("YmdHis");
     }
 
     private function _check_input(): void
@@ -34,13 +38,17 @@ final class ModulesService extends AppService implements IConsole
         $tables = $this->schema->get_tables();
         $tables = array_column($tables,"table_name");
         if (!in_array($this->input, $tables))
-            $this->_exception("not valid table: {$this->input}");
+            $this->_exception("not valid table: {$this->input}. Valid are: ".implode(", ",$tables));
     }
+
+
 
     //php run.php modules <table-name> o
     //run modules <table-name> (en ssh-be)
     public function run(): void
     {
-
+        mkdir($this->pathbuild);
+        $fields = $this->schema->get_fields_info($this->input);
+        $this->_pr($fields);
     }
 }
