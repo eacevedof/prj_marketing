b80822dad5ad89d3b00fe60e33358b1a812843a2
ioedumac <eacevedof@yahoo.es>
2022-01-17 21:45:17 +0100

    preparo %DT_COLUMNS%

--- backend_web/src/xxx-module/files/XxxsSearchService.php
+++ backend_web/src/xxx-module/files/XxxsSearchService.php
@@ -57,13 +57,8 @@ final class XxxsSearchService extends AppService
                 ->add_column("e_deletedby")->add_label(__("Deleted by"));
 
         $dthelp->add_column("uuid")->add_label(__("Code"))->add_tooltip(__("uuid"))
-            ->add_column("fullname")->add_label(__("Fullname"))
-            ->add_column("email")->add_label(__("Email"))
-            ->add_column("phone")->add_label(__("Phone"))
-            ->add_column("e_parent")->add_label(__("Superior"))
-            ->add_column("e_profile")->add_label(__("Profile"))
-            ->add_column("e_country")->add_label(__("Country"))
-            ->add_column("e_language")->add_label(__("Language"));
+            %DT_COLUMNS%
+        ;
 
         if($this->auth->is_root())
             $dthelp->add_action("show")
