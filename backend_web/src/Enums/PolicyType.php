c9ebf048c174e807505aaf050107bb6373c35e89
ioedumac <eacevedof@yahoo.es>
2022-01-22 12:19:16 +0100

    limpio codigo

--- backend_web/src/Enums/PolicyType.php
+++ backend_web/src/Enums/PolicyType.php
@@ -4,11 +4,10 @@ namespace App\Enums;
 
 abstract class PolicyType
 {
+    const DASHBOARD_READ = "dashboard:read";
+
     const USERS_READ = "users:read";
     const USERS_WRITE = "users:write";
-
-    const DASHBOARD_READ = "dashboard:read";
-    const DASHBOARD_WRITE = "dashboard:write";
 
     const PROMOTIONS_READ = "promotions:read";
     const PROMOTIONS_WRITE = "promotions:write";
