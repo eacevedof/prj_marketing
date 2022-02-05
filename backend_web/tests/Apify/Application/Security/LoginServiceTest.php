c0950417215476c5f39bfbca40615b165611b6f9
ioedumac <eacevedof@yahoo.es>
2022-02-05 15:10:37 +0100

    configurando test

--- backend_web/tests/Apify/Application/Security/LoginServiceTest.php
+++ backend_web/tests/Apify/Application/Security/LoginServiceTest.php
@@ -8,22 +8,22 @@ use App\Apify\Application\Security\LoginService;
 final class LoginServiceTest extends AbsTestBase
 {
 
-    public function test_get_token_nok()
+    public function test_get_token_nok(): void
     {
         $this->expectExceptionMessage("Domain localhost:200 is not authorized 2");
         $post=["user"=>"fulanito","password"=>"menganitox"];
-        $oServ = new LoginService("localhost:200",$post);
+        $oServ = new LoginService("localhost:200", $post);
         $oServ->get_token();
     }
 
-    public function test_get_token_nok_for_domain_asterisk()
+    public function test_get_token_nok_for_domain_asterisk(): void
     {
         $post=["user"=>"fulanito","password"=>"menganitox"];
         $oServ = new LoginService("*",$post);
         $token = $oServ->get_token();
     }
 
-    public function test_is_valid_token_ok()
+    public function test_is_valid_token_ok(): void
     {
         $post=["user"=>"fulanito","password"=>"menganitox"];
         $oServ = new LoginService("localhost:300",$post);
@@ -33,7 +33,7 @@ final class LoginServiceTest extends AbsTestBase
         $this->assertTrue($isvalid);
     }
 
-    public function test_valid_token_nok()
+    public function test_valid_token_nok(): void
     {
         $post=["user"=>"fulanito","password"=>"menganitox"];
         $oServ = new LoginService("localhost:300",$post);
