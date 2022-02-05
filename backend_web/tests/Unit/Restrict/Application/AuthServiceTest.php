433c42247b7f648cc50451a2b1b001b441310605
ioedumac <eacevedof@yahoo.es>
2022-02-05 20:24:32 +0100

    authservicetest

--- backend_web/tests/Unit/Restrict/Application/AuthServiceTest.php
+++ backend_web/tests/Unit/Restrict/Application/AuthServiceTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\Restrict\Auth\Application;
 
+use App\Shared\Infrastructure\Enums\ProfileType;
 use App\Shared\Infrastructure\Enums\SessionType;
 use Tests\Unit\AbsUnitTest;
 use App\Restrict\Auth\Application\AuthService;
@@ -30,31 +31,20 @@ final class AuthServiceTest extends AbsUnitTest
                     5 => "promotions:write",
                 ],
                 "lang" => "es",
-            ],
-            [
-                "id" => "2",
-                "fullname" => "Main Root ONE",
-                "description" => "Main Root ONE",
-                "email" => "eaf@eaf.com",
-                "secret" => "$2y$10\$BEClm.fzRU2shGk5nMLGRe4f0JnkXofGMBkLZ6sC86f8/aeetCMhC",
-                "id_language" => "2",
-                "id_profile" => "1",
-                "uuid" => "U00001",
-                "id_parent" => null,
-                "e_language" => "es",
-                "auth_user_permissions" => [
-                    0 => "dashboard:read",
-                    1 => "dashboard:write",
-                    2 => "users:read",
-                    3 => "users:write",
-                    4 => "promotions:read",
-                    5 => "promotions:write",
-                ],
-                "lang" => "es",
-            ],
+            ]
         ];
 
-        $_SESSION[SessionType::AUTH_USER] = $users[$i];
+        $_SESSION[SessionType::AUTH_USER] = $users[$i] ?? null;
+    }
+
+    public function test_get_null_user(): void
+    {
+        $this->log("test_get_null_user");
+        $this->_load_session(1);
+
+        $authService = AuthService::getme();
+        $user = $authService->get_user();
+        $this->assertTrue(count($user)===0);
     }
 
     public function test_get_user(): void
@@ -67,5 +57,6 @@ final class AuthServiceTest extends AbsUnitTest
         $this->assertArrayHasKey("id", $user);
         $this->assertNotNull($user["id"]);
     }
+
 
 }
