0915245595300158a14ced660189fa2818fd4766
ioedumac <eacevedof@yahoo.es>
2022-02-05 15:30:11 +0100

    configurando test en unit

--- backend_web/tests/Vendor/Theframework/Contexts/ContextsTest.php
+++ backend_web/tests/Vendor/Theframework/Contexts/ContextsTest.php
@@ -6,24 +6,26 @@ use TheFramework\Components\Db\Context\ComponentContext;
 
 final class ContextsTest extends AbsTestBase
 {
-    public function test_get_context()
+    public function test_get_context(): void
     {
+        $this->log("test_get_context");
         $oComp = new ComponentContext();
         $arConfig = $oComp->get_config();
-        $this->log($arConfig,"contexts.test_get_context");
         $this->assertEquals(true, is_array($arConfig));
     }
 
-    public function test_get_context_using_file()
+    public function test_get_context_using_file(): void
     {
+        $this->log("test_get_context_using_file");
         $sPathfile = __DIR__ . DIRECTORY_SEPARATOR;
         $oComp = new ComponentContext($sPathfile);
         $arConfig = $oComp->get_config();
         $this->assertEquals(true, is_array($arConfig));
     }
     
-    public function test_get_by_id()
+    public function test_get_by_id(): void
     {
+        $this->log("test_get_by_id");
         $oComp = new ComponentContext();
         $arConfig = $oComp->get_config();
         $this->assertEquals(true, is_array($arConfig));
