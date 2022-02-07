c7faff4f39fadd29bd65851296eebf204f4c8708
ioedumac <eacevedof@yahoo.es>
2022-02-07 23:38:46 +0100

    phinxlog

--- backend_web/db/phinx.php
+++ backend_web/db/phinx.php
@@ -7,7 +7,7 @@ return [
     ],
     "environments" => [
         "default_migration_table" => "phinxlog",
-        "default_database" => env("DB_ENV"),
+        "default_database" => "local",
         "production" => [
             "adapter" => "mysql",
             "host" => "host.docker.internal",
@@ -18,16 +18,16 @@ return [
             "charset" => env("DB_CHARSET", "utf8"),
             "table_prefix" => ""
         ],
-        "development" => [
-            "adapter" => env("DB_CONNECTION"),
-            "host" => env("DB_HOST"),
-            "name" => env("DB_DATABASE"),
-            "user" => env("DB_USERNAME"),
-            "pass" => env("DB_PASSWORD"),
-            "port" => env("DB_PORT", 3306),
+        "local" => [
+            "adapter" => "mysql",
+            "host" => "host.docker.internal",
+            "name" => "db_marketing",
+            "user" => "root",
+            "pass" => "1234",
+            "port" => 3306,
             "charset" => env("DB_CHARSET", "utf8"),
             "table_prefix" => ""
-        ]
+        ],
     ],
     "version_order" => "creation"
 ];
