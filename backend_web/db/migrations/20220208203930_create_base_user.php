b05afac12e92525ef04dcb023ad5a9bb0f6d7ade
ioedumac <eacevedof@yahoo.es>
2022-02-08 22:09:24 +0100

    create_base_user migraiton

--- backend_web/db/migrations/20220208203930_create_base_user.php
+++ backend_web/db/migrations/20220208203930_create_base_user.php
@@ -29,105 +29,105 @@ final class CreateBaseUser extends AbsMigration
         ->addColumn("uuid", "string", [
             "limit" => 50,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("code_erp", "string", [
             "limit" => 25,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("description", "string", [
             "limit" => 250,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("email", "string", [
             "limit" => 100,
             "null" => false,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("secret", "string", [
             "limit" => 100,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("phone", "string", [
             "limit" => 20,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("fullname", "string", [
             "limit" => 100,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("address", "string", [
             "limit" => 250,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("birthdate", "datetime", [
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("geo_location", "string", [
             "limit" => 500,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("id_parent", "integer", [
             "limit" => 11,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("id_gender", "integer", [
             "limit" => 11,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("id_nationality", "integer", [
             "limit" => 11,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("id_country", "integer", [
             "limit" => 11,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("id_language", "integer", [
             "limit" => 11,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("path_picture", "string", [
             "limit" => 100,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("id_profile", "integer", [
             "limit" => 11,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("token_reset", "string", [
             "limit" => 250,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("log_attempts", "integer", [
             "limit" => 5,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("date_validated", "datetime", [
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->addColumn("is_notifiable", "string", [
             "limit" => 4,
             "null" => true,
-            "default" => true,
+            "default" => null,
         ])
         ->create();
     }
