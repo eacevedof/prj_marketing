8219e3748d88cdbcc95c2212a839f773f23d4c27
ioedumac <eacevedof@yahoo.es>
2022-02-08 19:36:48 +0100

    create_base_array

---
+++ backend_web/db/migrations/20220208180153_create_base_array.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+use Phinx\Migration\AbstractMigration;
+
+final class CreateBaseArray extends AbstractMigration
+{
+    /*
+     * ``
+`update_platform`
+`update_user`
+`update_date`
+`delete_platform`
+`delete_user`
+`delete_date`
+`cru_csvnote`
+`is_erpsent`
+`is_enabled`
+`i`
+`id`
+`uuid``code_erp`
+`type`
+`id_tosave`
+`description`
+`order_by`
+     * */
+    public function up(): void
+    {
+        $this->table("base_arra", ["collation" => "utf8_general_ci"])
+            ->addColumn("processflag", "string", [
+                "limit" => 5,
+                "default" => null
+            ])
+            ->addColumn("insert_platform", "string", [
+                "limit" => 3,
+                "default" => 1
+            ])
+            ->addColumn("insert_user", "string", [
+                "limit" => 15,
+                "default" => null
+            ])
+            ->addColumn("insert_date", "datetime", [
+                "default" => "current_timestamp()",
+            ])
+            ->addColumn("update_user", "string", [
+                "limit" => 15,
+                "default" => null
+            ])
+            ->addColumn("update_date", "datetime", [
+                "default" => "current_timestamp()",
+            ])
+            ->create();
+    }
+
+    public function down(): void
+    {
+
+    }
+}
