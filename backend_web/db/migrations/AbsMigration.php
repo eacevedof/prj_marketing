61b9610fe980eab876dfd105029b0c6c14661f1e
ioedumac <eacevedof@yahoo.es>
2022-02-08 20:24:31 +0100

    add_sysfields

--- backend_web/db/migrations/AbsMigration.php
+++ backend_web/db/migrations/AbsMigration.php
@@ -11,51 +11,69 @@ abstract class AbsMigration extends AbstractMigration
     {
         $table->addColumn("processflag", "string", [
             "limit" => 5,
-            "default" => null
+            "default" => null,
+            "null" => true,
         ])
         ->addColumn("insert_platform", "string", [
             "limit" => 3,
-            "default" => 1
+            "default" => 1,
+            "null" => true,
         ])
         ->addColumn("insert_user", "string", [
             "limit" => 15,
-            "default" => null
+            "default" => null,
+            "null" => true,
         ])
         ->addColumn("insert_date", "datetime", [
+            "null" => true,
             "default" => "CURRENT_TIMESTAMP",
         ])
         ->addColumn("update_platform", "string", [
-            "limit" => 3
+            "limit" => 3,
+            "default" => null,
+            "null" => true,
         ])
         ->addColumn("update_user", "string", [
             "limit" => 15,
-            "default" => null
+            "default" => null,
+            "null" => true,
         ])
         ->addColumn("update_date", "datetime", [
             "default" => "CURRENT_TIMESTAMP",
+            "null" => true,
         ])
         ->addColumn("delete_platform", "string", [
-            "limit" => 3
+            "limit" => 3,
+            "default" => null,
+            "null" => true,
         ])
         ->addColumn("delete_user", "string", [
-            "limit" => 15
+            "limit" => 15,
+            "default" => null,
         ])
         ->addColumn("delete_date", "datetime", [
-            "default" => "CURRENT_TIMESTAMP",
+            "null" => true,
+            "default" => null,
         ])
         ->addColumn("cru_csvnote", "string", [
-            "limit" => 500
+            "limit" => 500,
+            "default" => null,
+            "null" => true,
         ])
         ->addColumn("is_erpsent", "string", [
             "limit" => 3,
-            "default" =>0
+            "default" =>0,
+            "null" => true,
         ])
         ->addColumn("is_enabled", "string", [
             "limit" => 3,
+            "null" => true,
             "default" => 1
         ])
         ->addColumn("i", "integer", [
-            "limit" => 11
+            "limit" => 11,
+            "dfault"=> null,
+            "null" => true,
         ]);
     }
 }
