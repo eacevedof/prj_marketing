354127f387b132186c98e8c57c8ffbe763e6f739
ioedumac <eacevedof@yahoo.es>
2022-02-12 17:56:14 +0100

    migracion create_app_promotion_user

--- backend_web/db/migrations/20220212160537_create_app_promotion_user.php
+++ backend_web/db/migrations/20220212160537_create_app_promotion_user.php
@@ -51,7 +51,6 @@ final class CreateAppPromotionUser extends AbsMigration
             "default" => null,
         ])
         ->addColumn("birthdate", "datetime", [
-            "limit" => 20,
             "null" => true,
             "default" => null,
         ])
