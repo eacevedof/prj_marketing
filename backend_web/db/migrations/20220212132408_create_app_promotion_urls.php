f70dee2b4307b0c55e1cbc7b79251afb469583ab
ioedumac <eacevedof@yahoo.es>
2022-02-12 14:38:54 +0100

    migracion app_promotion_url

---
+++ backend_web/db/migrations/20220212132408_create_app_promotion_urls.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+use Migrations\AbsMigration;
+
+final class CreateAppPromotionUrls extends AbsMigration
+{
+    private string $tablename = "app_promotion_urlsx";
+
+    public function up(): void
+    {
+        $this->_create_table();
+        $this->_initial_load();
+    }
+
+    private function _create_table(): void
+    {
+        $table = $this->table("{$this->tablename}", [
+            "collation" => "utf8_general_ci",
+            "id"=> false,
+            "primary_key" => ["id"]
+        ]);
+
+        $this->add_sysfields_min($table);
+
+        $this->add_fixed_fields($table);
+
+        $table->addColumn("id_promotion", "integer", [
+            "limit" => 11,
+            "null" => false,
+        ])
+        ->addColumn("id_type", "integer", [
+            "limit" => 11,
+            "null" => false,
+            "comment" => "app_array.type='promotion' ticktock, fb, twitter, instagram, other",
+        ])
+        ->addColumn("url_design", "string", [
+            "limit" => 500,
+            "null" => true,
+            "default" => null,
+            "comment" => "la url del diseño en la red social"
+        ])
+        ->addColumn("url_promotion", "string", [
+            "limit" => 1000,
+            "null" => true,
+            "default" => null,
+            "comment" => "la url de la promoción en la red social"
+        ])
+        ->addColumn("notes", "string", [
+            "limit" => 300,
+            "null" => true,
+            "default" => null,
+            "comment" => "notas relevantes, por ejemplo, hay que pasar el token=xxx"
+        ])
+        ->addColumn("is_active", "integer", [
+            "limit" => 2,
+            "null" => false,
+            "default" => 1,
+            "comment" => "para el futuro, indicará si hay que desactivar capturas desde esa publicacion"
+        ])
+        ->create();
+    }
+
+
+    private function _initial_load(): void
+    {
+    }
+
+    public function down(): void
+    {
+        $this->table($this->tablename)->drop()->save();
+    }
+}
