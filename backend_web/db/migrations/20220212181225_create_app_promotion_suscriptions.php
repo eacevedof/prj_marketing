27dbd6280b02eb29110c1afc009327d9470fb4d8
ioedumac <eacevedof@yahoo.es>
2022-02-12 19:58:03 +0100

    migracion create_app_promotion_suscriptions

--- backend_web/db/migrations/20220212181225_create_app_promotion_suscriptions.php
+++ backend_web/db/migrations/20220212181225_create_app_promotion_suscriptions.php
@@ -28,54 +28,54 @@ final class CreateAppPromotionSuscriptions extends AbsMigration
             "limit" => 11,
             "null" => false,
         ])
-            ->addColumn("id_language", "integer", [
-                "limit" => 11,
-                "null" => true,
-                "default" => null,
-                "comment" => "app_array.type='language' 1: english, 2:spanish",
-            ])
-            ->addColumn("id_country", "integer", [
-                "limit" => 11,
-                "null" => true,
-                "default" => null,
-                "comment" => "app_array.type='country'",
-            ])
-            ->addColumn("phone1", "string", [
-                "limit" => 20,
-                "null" => true,
-                "default" => null,
-            ])
-            ->addColumn("email", "string", [
-                "limit" => 100,
-                "null" => true,
-                "default" => null,
-            ])
-            ->addColumn("birthdate", "datetime", [
-                "null" => true,
-                "default" => null,
-            ])
-            ->addColumn("name1", "string", [
-                "limit" => 15,
-                "null" => true,
-                "default" => null,
-            ])
-            ->addColumn("name2", "string", [
-                "limit" => 15,
-                "null" => true,
-                "default" => null,
-            ])
-            ->addColumn("id_gender", "integer", [
-                "limit" => 2,
-                "null" => false,
-                "default" => 1,
-                "comment" => "app_array.type='gender', id_tosave:0: female, 1: male, 2:neutral",
-            ])
-            ->addColumn("address", "string", [
-                "limit" => 100,
-                "null" => true,
-                "default" => null,
-            ])
-            ->create();
+        ->addColumn("id_language", "integer", [
+            "limit" => 11,
+            "null" => true,
+            "default" => null,
+            "comment" => "app_array.type='language' 1: english, 2:spanish",
+        ])
+        ->addColumn("id_country", "integer", [
+            "limit" => 11,
+            "null" => true,
+            "default" => null,
+            "comment" => "app_array.type='country'",
+        ])
+        ->addColumn("phone1", "string", [
+            "limit" => 20,
+            "null" => true,
+            "default" => null,
+        ])
+        ->addColumn("email", "string", [
+            "limit" => 100,
+            "null" => true,
+            "default" => null,
+        ])
+        ->addColumn("birthdate", "datetime", [
+            "null" => true,
+            "default" => null,
+        ])
+        ->addColumn("name1", "string", [
+            "limit" => 15,
+            "null" => true,
+            "default" => null,
+        ])
+        ->addColumn("name2", "string", [
+            "limit" => 15,
+            "null" => true,
+            "default" => null,
+        ])
+        ->addColumn("id_gender", "integer", [
+            "limit" => 2,
+            "null" => false,
+            "default" => 1,
+            "comment" => "app_array.type='gender', id_tosave:0: female, 1: male, 2:neutral",
+        ])
+        ->addColumn("address", "string", [
+            "limit" => 100,
+            "null" => true,
+            "default" => null,
+        ])
+        ->create();
     }
 
     private function _initial_load(): void
