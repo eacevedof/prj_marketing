00e068fa75fb84ee07b58061984b01d18b9fc748
ioedumac <eacevedof@yahoo.es>
2022-02-13 18:47:14 +0100

    fixing user update

---
+++ backend_web/public/assets/js/restrict/users/permissions/update.js
@@ -0,0 +1,194 @@
+import { html, LitElement, css } from "/assets/js/vendor/lit.dev/lit-bundle.js"
+import get_cssrules from "/assets/js/common/cssrules.js"
+import injson from "/assets/js/common/req.js"
+import error from "/assets/js/common/fielderrors.js"
+import {SNACK} from "/assets/js/common/snackbar.js"
+import {cssformflex} from "/assets/js/common/formflex-lit-css.js"
+import {cssfielderror} from "/assets/js/common/fielderrors-lit-css.js"
+import {selector, get_formdata} from "/assets/js/common/shadowroot/shadowroot.js"
+
+const URL_UPDATE = "/restrict/user_permissionss/update"
+const ACTION = "user_permissionss.update"
+
+export class FormUserPermissionsUpdate extends LitElement {
+  static get styles() {
+    const globalStyle = css([get_cssrules([
+      "/themes/valex/assets/plugins/bootstrap/css/bootstrap.min.css",
+      "/themes/valex/assets/css/style.css",
+    ])])
+    return [
+      globalStyle,
+      cssformflex,
+      cssfielderror
+    ];
+  }
+
+  _$get(idsel) { return selector(this.shadowRoot)(idsel) }
+
+  _get_data() {
+    return get_formdata(this.shadowRoot)(this.fields)(["uuid"])
+  }
+
+  _on_cancel() {
+    window.modalraw.hide()
+  }
+
+  //1
+  constructor() {
+    super()
+    this.texts = {}
+    this.fields = {}
+  }
+
+  static properties = {
+    csrf: { type: String },
+    texts: {
+      converter: (strjson) => {
+        if (strjson) return JSON.parse(strjson)
+        return {}
+      },
+    },
+
+    fields: {
+      converter: (strjson) => {
+        if (strjson) return JSON.parse(strjson)
+        return {}
+      },
+    },
+
+    _issending: { type: Boolean, state: true },
+    _btnsend: { type: String, state: true },
+    _btncancel: { type: String, state: true },
+
+    _id: {type: String, state:true},
+    _uuid: {type: String, state:true},
+    _id_user: {type: String, state:true},
+    _json_rw: {type: String, state:true},
+  }
+
+  //2
+  requestUpdate() {
+    super.requestUpdate()
+  }
+
+  //3 (aqui siempre hay datos)
+  connectedCallback() {
+    super.connectedCallback()
+    this._issending = false
+    this._btnsend = this.texts.tr00
+    this._btncancel = this.texts.tr02
+
+    for (let p in this.fields) this["_".concat(p)] = this.fields[p]
+  }
+
+  //4
+  render() {
+    return html`
+      <form @submit=${this.on_submit}>
+        <div class="flex-row">
+          <div class="form-group">
+            <label for="id">${this.texts.f00}</label>
+            <div id="field-id">
+              <input type="text" id="id" .value=${this._id} class="form-control" maxlength="10">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="uuid">${this.texts.f01}</label>
+            <div id="field-uuid">
+              <input type="text" id="uuid" .value=${this._uuid} class="form-control" maxlength="50">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="id_user">${this.texts.f02}</label>
+            <div id="field-id_user">
+              <input type="text" id="id_user" .value=${this._id_user} class="form-control" maxlength="10">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="json_rw">${this.texts.f03}</label>
+            <div id="field-json_rw">
+              <input type="text" id="json_rw" .value=${this._json_rw} class="form-control" maxlength="2000">
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group mb-0">
+          <button id="btn-submit" ?disabled=${this._issending} class="btn btn-primary mt-3 mb-0">
+            ${this._btnsend}
+            ${this._issending
+                ? html`<img src="/assets/images/common/loading.png" width="25" height="25" />`
+                : html``
+            }
+          </button>
+          <button type="button" ?disabled=${this._issending} @click=${this.on_cancel} class="btn btn-secondary mt-3 mb-0">
+            ${this._btncancel}
+            ${this._issending
+                ? html`<img src="/assets/images/common/loading.png" width="25" height="25" />`
+                : html``
+            }
+          </button>
+        </div>
+      </form>
+    `
+  }
+
+  //5
+  firstUpdated() {
+    try {
+      this._$get("id").focus()
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+  //6
+  updated() {
+    //aqui se deberia des setear la prpiedad despues de una llamada async
+  }
+
+  async on_submit(e) {
+    e.preventDefault()
+    error.config({
+      wrapper: this.shadowRoot.querySelector("form"),
+      fields: Object.keys(this.fields)
+    })
+
+    this._issending = true
+    this._btnsend = this.texts.tr01
+    error.clear()
+
+    const response = await injson.put(
+        URL_UPDATE.concat(`/${this.fields.uuid}`), {
+          _action: ACTION,
+          _csrf: this.csrf,
+          uuid: this.fields.uuid,
+          ...this._get_data()
+        })
+
+    this._issending = false
+    this._btnsend = this.texts.tr00
+
+    if(response?.errors){
+      let errors = response.errors[0]?.fields_validation
+      if(errors) {
+        window.snack.set_time(4).set_inner(this.texts.tr03).set_color(SNACK.ERROR).show()
+        return error.append(errors)
+      }
+
+      errors = response?.errors
+      return window.snack.set_time(4).set_inner(errors.join("<br/>")).set_color(SNACK.ERROR).show()
+    }
+
+    const $dt = document.getElementById("table-datatable")
+    if ($dt) $($dt).DataTable().ajax.reload()
+    window.snack.set_time(4)
+      .set_color(SNACK.SUCCESS)
+      .set_inner(this.texts.tr04)
+      .show()
+
+  }//on_submit
+}
+
+if (!customElements.get("form-user_permissions-update"))
+  customElements.define("form-user_permissions-update", FormUserPermissionsUpdate)
