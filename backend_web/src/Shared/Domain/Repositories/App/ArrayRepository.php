4a8fafd2c42e042230a69723d6800ecbb3645826
ioedumac <eacevedof@yahoo.es>
2022-02-13 14:44:36 +0100

    fix arrayrepo

--- backend_web/src/Shared/Domain/Repositories/App/ArrayRepository.php
+++ backend_web/src/Shared/Domain/Repositories/App/ArrayRepository.php
@@ -30,9 +30,9 @@ final class ArrayRepository extends AppRepository
         if(!$idowner) $idowner = 0;
         $type = Types::PROMOTION;
         $sql = $this->_get_qbuilder()
-            ->set_comment("picklist.get_promotion_types")
+            ->set_comment("apparrayrepo.get_promotion_types")
             ->set_table("app_array as m")
-            ->set_getfields(["m.id","m.description"])
+            ->set_getfields(["m.id_pk as id","m.description"])
             ->add_and("m.is_enabled=1")
             ->add_and("m.delete_date IS NULL")
             ->add_and("m.type='$type'")
@@ -49,9 +49,9 @@ final class ArrayRepository extends AppRepository
     {
         $type = Types::LANGUAGE;
         $sql = $this->_get_qbuilder()
-            ->set_comment("picklist.get_languages")
+            ->set_comment("apparrayrepo.get_languages")
             ->set_table("app_array as m")
-            ->set_getfields(["m.id","m.description"])
+            ->set_getfields(["m.id_pk as id","m.description"])
             ->add_and("m.is_enabled=1")
             ->add_and("m.delete_date IS NULL")
             ->add_and("m.type='$type'")
@@ -68,7 +68,7 @@ final class ArrayRepository extends AppRepository
     {
         $type = Types::COUNTRY;
         $sql = $this->_get_qbuilder()
-            ->set_comment("picklist.get_countries")
+            ->set_comment("apparrayrepo.get_countries")
             ->set_table("app_array as m")
             ->set_getfields(["m.id","m.description"])
             ->add_and("m.is_enabled=1")
@@ -83,34 +83,19 @@ final class ArrayRepository extends AppRepository
         return $this->_get_associative(["id","description"]);
     }
 
-    public function exists(int $id, string $type): bool
+    public function exists(int $id, string $type, string $pk="id"): bool
     {
         $sql = $this->_get_qbuilder()
-            ->set_comment("picklist.exists_erp")
+            ->set_comment("apparrayrepo.exists")
             ->set_table("app_array as m")
             ->set_getfields(["m.id"])
             ->add_and("m.is_enabled=1")
             ->add_and("m.delete_date IS NULL")
-            ->add_and("m.id=$id")
+            ->add_and("m.$pk=$id")
             ->add_and("m.type='$type'")
             ->select()->sql()
         ;
         return (bool) $this->db->query($sql, 0, 0);
-    }
-
-    public function exists_erp(string $codeerp, string $type): int
-    {
-        $sql = $this->_get_qbuilder()
-            ->set_comment("picklist.exists_erp")
-            ->set_table("app_array as m")
-            ->set_getfields(["m.id"])
-            ->add_and("m.is_enabled=1")
-            ->add_and("m.delete_date IS NULL")
-            ->add_and("m.code_erp='$codeerp'")
-            ->add_and("m.type='$type'")
-            ->select()->sql()
-        ;
-        return (int) $this->db->query($sql, 0, 0);
     }
 
 }//ArrayRepository
