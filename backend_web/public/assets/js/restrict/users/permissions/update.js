8a6ac3e29a3ba11a2ca9f115bc1c0ff0d5880d36
ioedumac <eacevedof@yahoo.es>
2022-02-14 20:21:09 +0100

    userpermission form ok

--- backend_web/public/assets/js/restrict/users/permissions/update.js
+++ backend_web/public/assets/js/restrict/users/permissions/update.js
@@ -7,6 +7,7 @@ import {cssformflex} from "/assets/js/common/formflex-lit-css.js"
 import {cssfielderror} from "/assets/js/common/fielderrors-lit-css.js"
 import {selector, get_formdata} from "/assets/js/common/shadowroot/shadowroot.js"
 
+alert("hola")
 const URL_UPDATE = "/restrict/user-permissions/update"
 const ACTION = "userpermissions.update"
 
@@ -190,5 +191,5 @@ export class FormUserPermissionsUpdate extends LitElement {
   }//on_submit
 }
 
-if (!customElements.get("form-user_permissions-update"))
-  customElements.define("form-user_permissions-update", FormUserPermissionsUpdate)
+if (!customElements.get("form-user-permissions-update"))
+  customElements.define("form-user-permissions-update", FormUserPermissionsUpdate)
