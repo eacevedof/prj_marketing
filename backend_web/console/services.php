ec3439b36fbccad0b43469b678d39d8674f50cd4
ioedumac <eacevedof@yahoo.es>
2022-02-19 15:03:41 +0100

    trs

--- backend_web/console/services.php
+++ backend_web/console/services.php
@@ -19,11 +19,12 @@ return [
 
     "build-module"           => [
         "service"   =>  "App\\Console\\Application\\Dev\\Builder\\ModuleBuilderService",
-        "info"      =>  "",
+        "info"      =>  "como parametro se le pasa el nombre de la tabla",
     ],
 
     "get-translation"        => [
         "service"   =>  "App\\Console\\Application\\Dev\\Translation\\TranslationService",
-        "info"      =>  "",
+        "info"      =>  "--not-used los que hay en el .po pero no se invocan\n\t--repeated los que estan duplicados en el po".
+                        "\n\tsin parametros recupera todas las traducciones que no existen en default.po",
     ],
 ];
