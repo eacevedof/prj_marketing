ccb2d3e554b5ba84e321b90be165eeb08a41a2e4
ioedumac <eacevedof@yahoo.es>
2022-02-27 17:59:01 +0100

    pref grid save

--- backend_web/public/assets/js/restrict/users/preferences/update.js
+++ backend_web/public/assets/js/restrict/users/preferences/update.js
@@ -7,7 +7,7 @@ import {cssformflex} from "/assets/js/common/formflex-lit-css.js"
 import {cssfielderror} from "/assets/js/common/fielderrors-lit-css.js"
 import {selector, get_formdata} from "/assets/js/common/shadowroot/shadowroot.js"
 
-const URL_INSERT = "/restrict/users/:uuid/preferences/insert"
+const URL_INSERT = "/restrict/users/:uuid/preferences/update"
 const URL_UPDATE = "/restrict/users/:uuid/preferences/update"
 
 const ACTION = "userpreferences.update"
