d7730cfd0c470f7124d7bb67dfd0b7fd3ddbb6bc
ioedumac <eacevedof@yahoo.es>
2022-03-20 15:46:43 +0100

    fix session en open controller

--- backend_web/src/Shared/Infrastructure/Controllers/Open/OpenController.php
+++ backend_web/src/Shared/Infrastructure/Controllers/Open/OpenController.php
@@ -13,19 +13,24 @@ use App\Shared\Infrastructure\Controllers\AppController;
 use App\Shared\Infrastructure\Traits\RequestTrait;
 use App\Shared\Infrastructure\Traits\ViewTrait;
 use App\Shared\Infrastructure\Traits\ResponseTrait;
+use App\Shared\Infrastructure\Traits\SessionTrait;
+use App\Shared\Domain\Enums\SessionType;
 
 abstract class OpenController extends AppController
 {
     use RequestTrait;
     use ViewTrait;
     use ResponseTrait;
+    use SessionTrait;
 
     public function __construct()
     {
         $this->_load_request();
         $this->_load_view();
         $this->_load_response();
-        $this->set_layout("open/open");
+        $this->_load_session();
+        $this->set_layout("open/open")
+            ->add_var("authuser", $this->session->get(SessionType::AUTH_USER));
     }
 
 }//OpenController
