4187b28272b725fee4bc1b9361da05860da10541
ioedumac <eacevedof@yahoo.es>
2022-03-21 22:46:33 +0100

    no sesion response

--- backend_web/src/Restrict/Users/Infrastructure/Controllers/UsersSearchController.php
+++ backend_web/src/Restrict/Users/Infrastructure/Controllers/UsersSearchController.php
@@ -59,6 +59,12 @@ final class UsersSearchController extends RestrictController
     //@get
     public function search(): void
     {
+        if (!$this->auth->get_user())
+            $this->_get_json()
+                ->set_code(ResponseType::UNAUTHORIZED)
+                ->set_error([__("Your session has finished please re-login")])
+                ->show();
+
         if (!$this->request->is_accept_json())
             $this->_get_json()
                 ->set_code(ResponseType::BAD_REQUEST)
