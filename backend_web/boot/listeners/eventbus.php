e463778e449acc21f896ed829b90c3c7825eef9a
ioedumac <eacevedof@yahoo.es>
2022-05-01 00:08:02 +0200

    userpermissionsinsertservicelistener

--- backend_web/boot/listeners/eventbus.php
+++ backend_web/boot/listeners/eventbus.php
@@ -4,6 +4,7 @@
  */
 use App\Shared\Infrastructure\Bus\EventBus;
 use App\Restrict\UserPreferences\Application\UserPreferencesInsertService;
-
+use \App\Restrict\UserPermissions\Application\UserPermissionsInsertService;
 $bus = EventBus::instance();
 $bus->subscribe(new UserPreferencesInsertService());
+$bus->subscribe(new UserPermissionsInsertService());
---
+++ backend_web/src/Restrict/UserPermissions/Application/UserPermissionsInsertService.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Restrict\UserPermissions\Application;
+
+use App\Shared\Infrastructure\Services\AppService;
+use App\Shared\Domain\Bus\Event\IEventSubscriber;
+use App\Shared\Domain\Bus\Event\IEvent;
+use App\Shared\Infrastructure\Factories\EntityFactory as MF;
+use App\Shared\Infrastructure\Factories\RepositoryFactory as RF;
+use App\Shared\Infrastructure\Factories\ServiceFactory as SF;
+use App\Restrict\Auth\Application\AuthService;
+use App\Restrict\Users\Domain\UserPermissionsEntity;
+use App\Restrict\Users\Domain\UserPermissionsRepository;
+use App\Restrict\Users\Domain\Enums\UserPermissionType;
+use App\Restrict\Users\Domain\Events\UserWasCreated;
+use App\Shared\Infrastructure\Traits\RequestTrait;
+
+final class UserPermissionsInsertService extends AppService implements IEventSubscriber
+{
+    use RequestTrait;
+
+    private AuthService $auth;
+    private UserPermissionsEntity $userpermission;
+    private UserPermissionsRepository $repouserpermissions;
+
+    public function __construct()
+    {
+        $this->auth = SF::get_auth();
+        $this->userpermission = MF::get(UserPermissionsEntity::class);
+        $this->repouserpermissions = RF::get(UserPermissionsRepository::class);
+    }
+
+    public function on_event(IEvent $domevent): IEventSubscriber
+    {
+        if(get_class($domevent)!==UserWasCreated::class) return $this;
+
+        $permission = [
+            "id_user" => $domevent->aggregate_id(),
+            "uuid" => uniqid(),
+            "json_rw" => "[]",
+        ];
+
+        $this->userpermission->add_sysinsert($permission, $this->auth->get_user()["id"]);
+        $this->repouserpermissions->insert($permission);
+        return $this;
+    }
+}
