6e05cb7f4df08fcbbac01a3bbd68dcebb1bb184b
ioedumac <eacevedof@yahoo.es>
2022-05-02 16:26:36 +0200

    add rules by ui

--- backend_web/src/Open/PromotionCaps/Application/PromotionCapsInsertService.php
+++ backend_web/src/Open/PromotionCaps/Application/PromotionCapsInsertService.php
@@ -1,13 +1,18 @@
 <?php
 namespace App\Open\PromotionCaps\Application;
 
+use App\Checker\Application\CheckerService;
 use App\Open\PromotionCaps\Domain\PromotionCapSubscriptionsRepository;
+use App\Open\PromotionCaps\Domain\PromotionCapUsersEntity;
 use App\Open\PromotionCaps\Domain\PromotionCapUsersRepository;
 use App\Restrict\Promotions\Domain\PromotionRepository;
 use App\Restrict\Promotions\Domain\PromotionUiRepository;
+use App\Shared\Domain\Entities\FieldsValidator;
 use App\Shared\Domain\Repositories\App\ArrayRepository;
 use App\Shared\Infrastructure\Components\Date\UtcComponent;
+use App\Shared\Infrastructure\Factories\Specific\ValidatorFactory as VF;
 use App\Shared\Infrastructure\Services\AppService;
+use App\Shared\Infrastructure\Factories\EntityFactory as MF;
 use App\Shared\Infrastructure\Factories\RepositoryFactory as RF;
 use App\Restrict\Auth\Application\AuthService;
 use App\Restrict\BusinessData\Domain\BusinessDataRepository;
@@ -83,25 +88,73 @@ final class PromotionCapsInsertService extends AppService
 
     private function _load_promotionui(): void
     {
-        $this->promotionui = $this->repopromotionui->get_by_promotion((int) $this->promotion["id"]);
+        $this->promotionui = $this->repopromotionui->get_by_promotion($this->promotion["id"]);
         if (!$this->promotionui)
             $this->_exception(__("Missing promotion UI configuration!"), ExceptionType::CODE_FAILED_DEPENDENCY);
     }
 
+    private function _add_rules(): FieldsValidator
+    {
+        $this->validator = VF::get($this->input, $capuser = MF::get(PromotionCapUsersEntity::class));
 
+        $this->validator
+            ->add_rule("id_owner", "empty", function ($data) {
+                if ($this->auth->is_system() && !trim($data["value"]))
+                    return __("Empty field is not allowed");
+                return false;
+            })
+            ->add_rule("description", "empty", function ($data) {
+                return $data["value"] ? false : __("Empty field is not allowed");
+            })
+            ->add_rule("bgimage_xs", "bgimage_xs", function ($data) {
+                if (!$value = $data["value"]) return false;
+                if (!CheckerService::is_valid_url($value)) return __("Invalid url format");
+            })
+            ->add_rule("bgimage_sm", "bgimage_sm", function ($data) {
+                if (!$value = $data["value"]) return false;
+                if (!CheckerService::is_valid_url($value)) return __("Invalid url format");
+            })
+            ->add_rule("bgimage_md", "bgimage_md", function ($data) {
+                if (!$value = $data["value"]) return false;
+                if (!CheckerService::is_valid_url($value)) return __("Invalid url format");
+            })
+            ->add_rule("bgimage_lg", "bgimage_lg", function ($data) {
+                if (!$value = $data["value"]) return false;
+                if (!CheckerService::is_valid_url($value)) return __("Invalid url format");
+            })
+            ->add_rule("bgimage_xl", "bgimage_xl", function ($data) {
+                if (!$value = $data["value"]) return false;
+                if (!CheckerService::is_valid_url($value)) return __("Invalid url format");
+            })
+            ->add_rule("bgimage_xxl", "bgimage_xxl", function ($data) {
+                if (!$value = $data["value"]) return false;
+                if (!CheckerService::is_valid_url($value)) return __("Invalid url format");
+            })
+            ->add_rule("id_tz", "id_tz", function ($data) {
+                return $data["value"] ? false : __("Empty field is not allowed");
+            })
+            ->add_rule("date_from", "date_from", function ($data) {
+                if (!$value = $data["value"]) return __("Empty field is not allowed");
+                if (!$this->datecomp->set_date1($value)->is_valid()) return __("Invalid date {0}", $value);
+                if ($value>$data["data"]["date_to"]) return __("Date from is greater than Date to");
+                return false;
+            })
+            ->add_rule("date_to", "date_to", function ($data) {
+                if (!$value = $data["value"]) return __("Empty field is not allowed");
+                if (!$this->datecomp->set_date1($value)->is_valid()) return __("Invalid date {0}", $value);
+                if ($value<$data["data"]["date_from"]) return __("Date to is lower than Date from");
+                return false;
+            });
+
+        return $this->validator;
+    }
     public function __invoke(): array
     {
         $this->_load_promotion();
         $this->_load_promotionui();
 
         return [
-            "businessdata" => $this->businesssdata,
-            "promotion" => $this->promotion,
-            "promotionui" => $this->promotionui,
 
-            "metadata" => [
-
-            ], //depende si es test o no
         ];
     }
 }
