540ef8de785073b2641a48bd608e7510d9d95f6b
ioedumac <eacevedof@yahoo.es>
2022-05-02 12:07:32 +0200

    shadowroot.js

--- backend_web/public/assets/js/common/shadowroot/shadowroot.js
+++ backend_web/public/assets/js/common/shadowroot/shadowroot.js
@@ -2,8 +2,8 @@ export const selector = shadow => strsel => shadow.querySelector(`#${strsel}`)
 
 //arexclude evita que se envie esa key
 export const  get_formdata = shadow => fields => (arexclude=[]) =>{
-  const data = Object.keys(fields)
-    .map(field => {
+  const fieldids = Array.isArray(fields) ? fields : Object.keys(fields)
+  const data = fieldids.map(field => {
       const ob = {}
       if (arexclude.includes(field)) return {}
       ob[field] = selector(shadow)(field)?.value ?? ""
