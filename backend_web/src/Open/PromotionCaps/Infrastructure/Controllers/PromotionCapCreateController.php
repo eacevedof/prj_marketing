ddea98b8ef4618d633e967d17915978c03c966c6
ioedumac <eacevedof@yahoo.es>
2022-05-03 21:56:41 +0200

    fix promotioncap users

--- backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapCreateController.php
+++ backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapCreateController.php
@@ -8,8 +8,6 @@ namespace App\Open\PromotionCaps\Infrastructure\Controllers;
 use App\Picklist\Application\PicklistService;
 use App\Shared\Domain\Enums\ResponseType;
 use App\Shared\Infrastructure\Controllers\Open\OpenController;
-use App\Shared\Infrastructure\Exceptions\ForbiddenException;
-use App\Shared\Infrastructure\Exceptions\NotFoundException;
 use App\Shared\Infrastructure\Factories\ServiceFactory as SF;
 use App\Open\PromotionCaps\Application\PromotionCapInfoService;
 use App\Shared\Domain\Enums\PageType;
@@ -45,20 +43,6 @@ final class PromotionCapCreateController extends OpenController
                 ->add_var(PageType::H1, __("Error!"))
                 ->add_var("error", $e->getMessage())
                 ->render();
-        }
-        catch (NotFoundException $e) {
-            $this->add_header(ResponseType::NOT_FOUND)
-                ->add_var(PageType::H1, $e->getMessage())
-                ->set_foldertpl("Open/Errors/Infrastructure/Views")
-                ->set_template("404")
-                ->render_nl();
-        }
-        catch (ForbiddenException $e) {
-            $this->add_header(ResponseType::FORBIDDEN)
-                ->add_var(PageType::H1, $e->getMessage())
-                ->set_foldertpl("Open/Errors/Infrastructure/Views")
-                ->set_template("403")
-                ->render_nl();
         }
         catch (Exception $e) {
             $this->add_header(ResponseType::INTERNAL_SERVER_ERROR)
