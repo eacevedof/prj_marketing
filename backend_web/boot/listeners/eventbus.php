2215e734ba3d01291a1e521c181d4341382c47e7
ioedumac <eacevedof@yahoo.es>
2022-05-04 22:19:39 +0200

    action event

--- backend_web/boot/listeners/eventbus.php
+++ backend_web/boot/listeners/eventbus.php
@@ -7,9 +7,10 @@ use App\Restrict\UserPreferences\Application\UserPreferencesInsertService;
 use App\Restrict\UserPermissions\Application\UserPermissionsInsertService;
 use App\Restrict\PromotionsUi\Application\PromotionUiInsertService;
 use App\Open\PromotionCaps\Application\PromotionCapSubscriptionEventHandler;
-
+use App\Open\PromotionCaps\Application\PromotionCapActionEventHandler;
 $bus = EventBus::instance();
 $bus->subscribe(new UserPreferencesInsertService());
 $bus->subscribe(new UserPermissionsInsertService());
 $bus->subscribe(new PromotionUiInsertService());
 $bus->subscribe(new PromotionCapSubscriptionEventHandler());
+$bus->subscribe(new PromotionCapActionEventHandler());
