e53ed762bf66695bc5b1d3fcd5d9855cb51dc824
ioedumac <eacevedof@yahoo.es>
2022-05-04 21:11:42 +0200

    subscription ok

---
+++ backend_web/src/Open/PromotionCaps/Application/PromotionCapActionEventHandler.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Open\PromotionCaps\Application;
+
+use App\Open\PromotionCaps\Domain\Events\PromotionCapUserWasCreatedEvent;
+use App\Open\PromotionCaps\Domain\PromotionCapSubscriptionEntity;
+use App\Open\PromotionCaps\Domain\PromotionCapSubscriptionsRepository;
+use App\Restrict\Auth\Application\AuthService;
+use App\Shared\Infrastructure\Services\AppService;
+use App\Shared\Domain\Bus\Event\IEventSubscriber;
+use App\Shared\Domain\Bus\Event\IEvent;
+use App\Shared\Infrastructure\Factories\EntityFactory as MF;
+use App\Shared\Infrastructure\Factories\RepositoryFactory as RF;
+
+final class PromotionCapActionEventHandler extends AppService implements IEventSubscriber
+{
+    public function on_event(IEvent $domevent): IEventSubscriber
+    {
+        if(get_class($domevent)!==PromotionCapUserWasCreatedEvent::class) return $this;
+
+        $subscription = [
+            "id_promouser" => $domevent->aggregate_id(),
+            "uuid" => uniqid(),
+            "id_owner" => $domevent->id_owner(),
+            "id_promotion" => $domevent->id_promotion(),
+            "remote_ip" => $domevent->remote_ip(),
+            "date_subscription" => $domevent->date_subscription(),
+            "code_execution" => uniqid()
+        ];
+
+        $iduser = AuthService::getme()->get_user()["id"] ?? -1;
+        MF::get(PromotionCapSubscriptionEntity::class)->add_sysinsert($subscription, $iduser);
+        RF::get(PromotionCapSubscriptionsRepository::class)->insert($subscription);
+        return $this;
+    }
+}
