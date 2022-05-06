84cf59aeba35c56bde7f3e05f6de659f39f49d81
ioedumac <eacevedof@yahoo.es>
2022-05-06 19:54:52 +0200

    confirm service

--- backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapsConfirmController.php
+++ backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapsConfirmController.php
@@ -13,6 +13,7 @@ use App\Open\PromotionCaps\Application\PromotionCapsConfirmService;
 use App\Open\PromotionCaps\Domain\Enums\RequestActionType;
 use App\Shared\Domain\Enums\PageType;
 use App\Shared\Infrastructure\Exceptions\FieldsException;
+use App\Open\PromotionCaps\Domain\Errors\PromotionCapException;
 
 final class PromotionCapsConfirmController extends OpenController
 {
@@ -22,22 +23,27 @@ final class PromotionCapsConfirmController extends OpenController
             $this->set_layout("open/empty")
                 ->add_header(ResponseType::BAD_REQUEST)
                 ->add_var(PageType::H1, __("Bad Request"))
-                ->add_var("description", __("Missing promotion code and/or subscription code"))
+                ->add_var("description", __("Missing promotion and/or subscription code"))
                 ->set_foldertpl("Open/Errors/Infrastructure/Views")
-                ->set_template("400");
-
+                ->set_template("400")
+                ->render();
         try {
             $insert = SF::get_callable(PromotionCapsConfirmService::class, [
                 "promouuid" => $promouuid,
                 "subsuuid" => $subsuuid
             ]);
             $result = $insert();
-            $this->set_layout("open/empty")
-                ->add_var(PageType::H1, htmlentities($result["promotion"]))
+            $this->add_var(PageType::H1, htmlentities($result["promotion"]))
                 ->add_var("result", $result);
 
             unset($insert, $result, $promouuid, $subsuuid);
-            $this->view->render();
+            $this->view->render_nl();
+        }
+        catch (PromotionCapException $e) {
+            $this->add_header($e->getCode())
+                ->add_var(PageType::H1, __("Warning!"))
+                ->add_var("error", $e->getMessage())
+                ->render_nl();
         }
         catch (Exception $e) {
             $this->add_header(ResponseType::INTERNAL_SERVER_ERROR)
--- backend_web/src/Shared/Infrastructure/Views/layouts/open/empty.tpl
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @var App\Shared\Infrastructure\Views\AppView $this
- */
-$this->_template();
-?>
-<!-- empty.tpl -->
