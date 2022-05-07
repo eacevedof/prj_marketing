e095faefa8b8002808e0493b5f8d61e89bb24baf
ioedumac <eacevedof@yahoo.es>
2022-05-07 19:50:30 +0200

    on confirmation email

--- backend_web/src/Open/PromotionCaps/Application/PromotionSubscriptionNotifierEventHandler.php
+++ backend_web/src/Open/PromotionCaps/Application/PromotionSubscriptionNotifierEventHandler.php
@@ -48,7 +48,7 @@ final class PromotionSubscriptionNotifierEventHandler extends AppService impleme
         if (!is_file($pathtpl)) throw new \Exception("bad path $path");
 
         $data = RF::get(PromotionCapUsersRepository::class)->get_subscription_data($domevent->aggregate_id());
-        $data["confirm_link"] = "http://localhost:900/points/{$data["businesscode"]}/user/{$data["subscode"]}";
+        $data["confirm_link"] = "http://localhost:900/points/{$data["businesscode"]}/user/{$data["capusercode"]}";
         $html = FromTemplate::get_content($pathtpl, ["data"=>$data]);
         print_r($html);
         /**
