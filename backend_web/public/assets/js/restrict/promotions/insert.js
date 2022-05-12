5873e4c5c4cd6caa8031cf398bac2a0014be7717
ioedumac <eacevedof@yahoo.es>
2022-05-12 19:07:16 +0200

    promotion insert mejora codigo

--- backend_web/public/assets/js/restrict/promotions/insert.js
+++ backend_web/public/assets/js/restrict/promotions/insert.js
@@ -6,7 +6,7 @@ import {SNACK} from "/assets/js/common/snackbar.js"
 import {cssformflex} from "/assets/js/common/formflex-lit-css.js"
 import {cssfielderror} from "/assets/js/common/fielderrors-lit-css.js"
 import {selector, get_formdata} from "/assets/js/common/shadowroot/shadowroot.js"
-import {get_link, get_img_link} from "/assets/js/common/html/link.js"
+import {get_img_link} from "/assets/js/common/html/link.js"
 
 const URL_POST = "/restrict/promotions/insert"
 const ACTION = "promotions.insert"
