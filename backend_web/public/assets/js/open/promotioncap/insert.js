bb23a3f1b011f6397fb386771aa22db4662b3d42
ioedumac <eacevedof@yahoo.es>
2022-06-25 22:02:11 +0200

    mensaje de confirmación

--- backend_web/public/assets/js/open/promotioncap/insert.js
+++ backend_web/public/assets/js/open/promotioncap/insert.js
@@ -387,10 +387,12 @@ export class FormPromotionCapInsert extends LitElement {
       }
     }
     const $section = window.document.querySelector(".section")
+    let name = this._$get("input-name1").value
+    name = name[0].toUpperCase().concat(name.slice(1))
     $section.innerHTML = `
     <div class="subscription-message">
       <p>
-      Thank you <b>${this._$get("input-name1").value}</b> for you subscription. Please check your email and press the confirm button.
+      Thank you <b>${name}</b> for your subscription. Please check your email and press the confirm button.
       </p>
     </div>`
 
