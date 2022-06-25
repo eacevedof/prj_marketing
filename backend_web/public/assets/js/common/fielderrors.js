353288b008bea83c130d048ca6cba2d98b135285
ioedumac <eacevedof@yahoo.es>
2022-06-25 19:51:40 +0200

    ya coloca bien el div de error

--- backend_web/public/assets/js/common/fielderrors.js
+++ backend_web/public/assets/js/common/fielderrors.js
@@ -43,11 +43,18 @@ const _append = errors => {
   const inform = []
   onlymsgs.forEach(objfield => {
     const lis = objfield.messages.map(message => `<li>${message}</li>`).join("")
-    const html = tpl.replace("%lis%",lis).replace("%css%","")
+    const divtpl = tpl.replace("%lis%",lis).replace("%css%","")
     const $input = _$wrapper.querySelector(`#${objfield.fieldid}`)
     if ($input) {
       inform.push(objfield.fieldid)
-      $input.insertAdjacentHTML("afterend", html)
+      if ($input.type!=="checkbox") {
+        $input.insertAdjacentHTML("afterend", divtpl)
+      }
+      else {
+        const $label = $input.parentElement
+        $label.insertAdjacentHTML("afterend", divtpl)
+      }
+
       $input.classList.add(CSS.INPUT_ERROR)
       return
     }
