854d53563b4674ba27f7f80f2f98abc50d63488e
ioedumac <eacevedof@yahoo.es>
2022-06-28 22:47:32 +0200

    send mail

--- backend_web/src/Open/PromotionCaps/Application/PromotionSubscriptionNotifierEventHandler.php
+++ backend_web/src/Open/PromotionCaps/Application/PromotionSubscriptionNotifierEventHandler.php
@@ -42,7 +42,7 @@ final class PromotionSubscriptionNotifierEventHandler extends AppService impleme
         $email = CF::get(FuncEmailComponent::class);
         $email
             ->set_from("elchalanaua@gmail.com")
-            ->add_to("eacevedof@yahoo.es")
+            ->add_to("eacevedof@gmail.com")
             ->set_subject(__("Subscription to \"{0}\"", $data["promotion"]))
             ->set_content($html)
             ->send()
