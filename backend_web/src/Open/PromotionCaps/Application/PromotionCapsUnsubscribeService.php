75779f2e622387256a6e1d7637340773e0a0be16
ioedumac <eacevedof@yahoo.es>
2022-06-29 20:07:01 +0200

    unsubscribe skeleton

---
+++ backend_web/src/Open/PromotionCaps/Application/PromotionCapsUnsubscribeService.php
@@ -0,0 +1,124 @@
+<?php
+namespace App\Open\PromotionCaps\Application;
+
+use App\Open\PromotionCaps\Domain\Enums\PromotionCapActionType;
+use App\Open\PromotionCaps\Domain\Errors\PromotionCapException;
+use App\Open\PromotionCaps\Domain\Events\PromotionCapActionHasOccurredEvent;
+use App\Open\PromotionCaps\Domain\Events\PromotionCapConfirmedEvent;
+use App\Open\PromotionCaps\Domain\PromotionCapSubscriptionEntity;
+use App\Open\PromotionCaps\Domain\PromotionCapSubscriptionsRepository;
+use App\Open\PromotionCaps\Domain\PromotionCapUsersRepository;
+use App\Restrict\Promotions\Domain\PromotionRepository;
+use App\Shared\Domain\Bus\Event\IEventDispatcher;
+use App\Shared\Domain\Enums\ExceptionType;
+use App\Shared\Infrastructure\Bus\EventBus;
+use App\Shared\Infrastructure\Factories\ServiceFactory as SF;
+use App\Shared\Infrastructure\Services\AppService;
+use App\Shared\Infrastructure\Factories\EntityFactory as MF;
+use App\Shared\Infrastructure\Factories\RepositoryFactory as RF;
+use App\Shared\Infrastructure\Factories\ComponentFactory as CF;
+use App\Restrict\Auth\Application\AuthService;
+use App\Shared\Infrastructure\Traits\RequestTrait;
+use App\Shared\Infrastructure\Components\Formatter\TextComponent;
+
+final class PromotionCapsUnsubscribeService extends AppService implements IEventDispatcher
+{
+    use RequestTrait;
+
+    private PromotionRepository $repopromotion;
+    private PromotionCapSubscriptionsRepository $repopromocapsubscription;
+    private PromotionCapUsersRepository $repopromocapuser;
+
+    private array $promotion;
+    private array $subscriptiondata;
+    private int $istest;
+
+    public function __construct(array $input)
+    {
+        $this->input = $input;
+        $this->istest = (int) $input["_test_mode"];
+        $this->repopromocapsubscription = RF::get(PromotionCapSubscriptionsRepository::class);
+        $this->repopromocapuser = RF::get(PromotionCapUsersRepository::class);
+        $this->repopromotion = RF::get(PromotionRepository::class);
+    }
+
+    private function _load_promotion(): void
+    {
+        $this->promotion = $this->repopromotion->get_by_uuid($this->input["promotionuuid"], [
+            "delete_date", "id", "uuid", "slug", "max_confirmed", "is_published", "is_launched", "id_tz",
+            "date_from", "date_to", "date_execution", "id_owner", "num_confirmed", "disabled_date"
+        ]);
+
+        SF::get(PromotionCapCheckService::class, [
+            "email" => ($this->input["email"] ?? ""),
+            "promotion" => $this->promotion,
+            "is_test" => $this->istest,
+            "user" => AuthService::getme()->get_user()
+        ])->is_suitable_or_fail();
+    }
+
+    private function _load_subscription(): void
+    {
+        $promosubscription = $this->repopromocapsubscription->get_by_uuid($this->input["subscriptionuuid"], ["id_promouser"]);
+        if(!$promosubscription)
+            $this->_promocap_exception(__("No subscription found"), ExceptionType::CODE_NOT_FOUND);
+
+        $this->subscriptiondata = $this->repopromocapuser->get_subscription_data($promosubscription["id_promouser"]);
+        if (!$this->subscriptiondata)
+            $this->_promocap_exception(__("No subscription data found"), ExceptionType::CODE_NOT_FOUND);
+
+        if ($this->subscriptiondata["promocode"]!==$this->promotion["uuid"])
+            $this->_promocap_exception(__("Promotion code does not match for this subscription"), ExceptionType::CODE_BAD_REQUEST);
+
+        if ($this->subscriptiondata["date_confirm"] || $this->subscriptiondata["date_execution"])
+            $this->_promocap_exception(__("You have already confirmed your subscription"), ExceptionType::CODE_BAD_REQUEST);
+    }
+
+    private function _promocap_exception(string $message, int $code = ExceptionType::CODE_INTERNAL_SERVER_ERROR): void
+    {
+        throw new PromotionCapException($message, $code);
+    }
+
+    private function _dispatch(array $payload): void
+    {
+        EventBus::instance()->publish(...[
+            PromotionCapConfirmedEvent::from_primitives($idcapuser = $this->subscriptiondata["idcapuser"], [
+                "subsuuid" => $this->subscriptiondata["subscode"],
+                "email" => $this->subscriptiondata["email"],
+                "date_confirm" => $payload["date_confirm"],
+            ]),
+
+            PromotionCapActionHasOccurredEvent::from_primitives(-1, [
+                "id_promotion" => $this->promotion["id"],
+                "id_promouser" => $idcapuser,
+                "id_type" => PromotionCapActionType::CONFIRMED,
+                "url_req" => $this->request->get_request_uri(),
+                "url_ref" => $this->request->get_referer(),
+                "remote_ip" => $this->request->get_remote_ip(),
+                "is_test" => $this->istest,
+            ])
+        ]);
+    }
+
+    public function __invoke(): array
+    {
+        $this->_load_request();
+        $this->_load_promotion();
+        $this->_load_subscription();
+        $this->repopromocapsubscription->set_model($entitysubs = MF::get(PromotionCapSubscriptionEntity::class));
+        $confirm = [
+            "id"=>$this->subscriptiondata["subsid"],
+            "uuid"=>$this->subscriptiondata["subscode"],
+            "date_confirm"=> $date = date("Y-m-d H:i:s"),
+            "code_execution" => CF::get(TextComponent::class)->get_random_word(4, 2),
+            "subs_status" => PromotionCapActionType::CONFIRMED
+        ];
+        $iduser = AuthService::getme()->get_user()["id"] ?? -1;
+        $entitysubs->add_sysupdate($confirm, $iduser);
+        $this->repopromocapsubscription->update($confirm);
+
+        $this->_dispatch(["date_confirm"=>$date]);
+
+        return $this->subscriptiondata;
+    }
+}
---
+++ backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapUnsubscribeController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @link eduardoaf.com
+ */
+namespace App\Open\PromotionCaps\Infrastructure\Controllers;
+
+use App\Shared\Domain\Enums\ResponseType;
+use App\Shared\Infrastructure\Controllers\Open\OpenController;
+use App\Shared\Infrastructure\Factories\ServiceFactory as SF;
+use App\Open\PromotionCaps\Application\PromotionCapsUnsubscribeService;
+use App\Shared\Domain\Enums\PageType;
+use App\Open\PromotionCaps\Domain\Errors\PromotionCapException;
+
+final class PromotionCapUnsubscribeController extends OpenController
+{
+    public function unsubscribe(string $promotionuuid, string $subscriptionuuid): void
+    {
+        if (!($promotionuuid && $subscriptionuuid))
+            $this->set_layout("open/empty")
+                ->add_header(ResponseType::BAD_REQUEST)
+                ->add_var(PageType::H1, __("Bad Request"))
+                ->add_var("description", __("Missing promotion and/or subscription code"))
+                ->set_foldertpl("Open/Errors/Infrastructure/Views")
+                ->set_template("400")
+                ->render();
+        try {
+            $insert = SF::get_callable(PromotionCapsUnsubscribeService::class, [
+                "promotionuuid" => $promotionuuid,
+                "subscriptionuuid" => $subscriptionuuid,
+                "_test_mode" => $this->request->get_get("mode", "")==="test",
+            ]);
+            $result = $insert();
+            $this->add_var(PageType::H1, htmlentities($result["promotion"]))
+                ->add_var("result", $result);
+
+            unset($insert, $result, $promotionuuid, $subscriptionuuid);
+            $this->view->render_nl();
+        }
+        catch (PromotionCapException $e) {
+            $this->add_header($e->getCode())
+                ->add_var(PageType::H1, __("Whoops!"))
+                ->add_var("error", $e->getMessage())
+                ->render_nl();
+        }
+        catch (Exception $e) {
+            $this->add_header(ResponseType::INTERNAL_SERVER_ERROR)
+                ->add_var(PageType::H1, $e->getMessage())
+                ->set_foldertpl("Open/Errors/Infrastructure/Views")
+                ->set_template("500")
+                ->render_nl();
+        }
+    }
+}
+
+
+
