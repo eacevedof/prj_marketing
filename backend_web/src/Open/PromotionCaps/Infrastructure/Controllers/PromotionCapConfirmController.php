6f4a83a7ea46578e035bc1dd3e807334deb872a4
ioedumac <eacevedof@yahoo.es>
2022-07-14 20:50:53 +0200

    open error layout

--- backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapConfirmController.php
+++ backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapConfirmController.php
@@ -18,7 +18,7 @@ final class PromotionCapConfirmController extends OpenController
         if (!($promotionuuid && $subscriptionuuid))
             $this->set_layout("open/error")
                 ->add_header($code = ResponseType::BAD_REQUEST)
-                ->add_var(PageType::TITLE, __("Subscription error!"))
+                ->add_var(PageType::TITLE, __("Subscription confirmation error!"))
                 ->add_var(PageType::H1, __("Bad Request"))
                 ->add_var("error", __("Missing promotion and/or subscription code"))
                 ->add_var("code", $code)
@@ -41,7 +41,7 @@ final class PromotionCapConfirmController extends OpenController
         catch (PromotionCapException $e) {
             $this->add_header($e->getCode())
                 ->set_layout("open/error")
-                ->add_var(PageType::TITLE, $title = __("Subscription error!"))
+                ->add_var(PageType::TITLE, $title = __("Subscription confirmation error!"))
                 ->add_var(PageType::H1, $title)
                 ->add_var("error", $e->getMessage())
                 ->add_var("code", $e->getCode())
@@ -50,7 +50,7 @@ final class PromotionCapConfirmController extends OpenController
         catch (Exception $e) {
             $this->add_header(ResponseType::INTERNAL_SERVER_ERROR)
                 ->set_layout("open/error")
-                ->add_var(PageType::TITLE, $title = __("Unexpected error"))
+                ->add_var(PageType::TITLE, $title = __("Subscription confirmation error!"))
                 ->add_var(PageType::H1, $title)
                 ->add_var("error", $e->getMessage())
                 ->add_var("code", $e->getCode())
