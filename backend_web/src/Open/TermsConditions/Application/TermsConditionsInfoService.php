75a70665a261baf275e4ddf2e5ae69b340896492
ioedumac <eacevedof@yahoo.es>
2022-07-15 23:50:16 +0200

    terms and conditions User a user

--- backend_web/src/Open/TermsConditions/Application/TermsConditionsInfoService.php
+++ backend_web/src/Open/TermsConditions/Application/TermsConditionsInfoService.php
@@ -92,10 +92,10 @@ final class TermsConditionsInfoService extends AppService
     public function get_by_promotion(): array
     {
         return array_merge([
-            ["h2" => "- ".__("Promotion Terms")],
+            ["h2" => "- ".__("Promotion Terms: {0}", "xxx")],
             ["h2" => "esto es un h2"],
             ["p" => "esto es una p"],
-            ["h2" => "- ".__("General Terms")]
+            ["h2" => "- ".__("Provider Terms")]
         ],
         $this->_general_terms(),
         );
