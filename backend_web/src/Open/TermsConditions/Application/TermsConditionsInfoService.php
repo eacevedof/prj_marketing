a1ce1cf41ff22fbf4416260fb26f2da83fe9fd95
ioedumac <eacevedof@yahoo.es>
2022-07-15 21:00:53 +0200

    terms conditions info

--- backend_web/src/Open/TermsConditions/Application/TermsConditionsInfoService.php
+++ backend_web/src/Open/TermsConditions/Application/TermsConditionsInfoService.php
@@ -2,9 +2,16 @@
 namespace App\Open\TermsConditions\Application;
 
 use App\Shared\Infrastructure\Services\AppService;
+use App\Shared\Infrastructure\Factories\RepositoryFactory;
+use App\Restrict\Promotions\Domain\PromotionRepository;
 
 final class TermsConditionsInfoService extends AppService
 {
+    public function __construct(array $input)
+    {
+        $this->input = $input["promoslug"] ?? "";
+    }
+
     public function __invoke(): array
     {
         return [];
@@ -12,6 +19,8 @@ final class TermsConditionsInfoService extends AppService
 
     public function get_by_promotion(): array
     {
-        return [];
+        return [
+            ["h2" => ""]
+        ];
     }
 }
--- backend_web/src/Open/TermsConditions/Infrastructure/Controllers/TermsConditionsInfoController.php
+++ backend_web/src/Open/TermsConditions/Infrastructure/Controllers/TermsConditionsInfoController.php
@@ -44,11 +44,11 @@ final class TermsConditionsInfoController extends OpenController
         }
     }
 
-    public function promotion(string $slug): void
+    public function promotion(string $promoslug): void
     {
         try {
-            $terms = SF::get(TermsConditionsInfoService::class)->get_by_promotion($slug);
-            $this->set_layout("open/business")
+            $terms = SF::get(TermsConditionsInfoService::class, ["promoslug"=>$promoslug])->get_by_promotion();
+            $this->set_layout("open/open")
                 ->add_var(PageType::TITLE, $title = __("Terms & Conditions"))
                 ->add_var(PageType::H1, $title)
                 ->add_var("result", $terms)
@@ -56,28 +56,31 @@ final class TermsConditionsInfoController extends OpenController
         }
         catch (NotFoundException $e) {
             $this->add_header(ResponseType::NOT_FOUND)
-                ->add_var(PageType::H1, $e->getMessage())
-                ->set_foldertpl("Open/Errors/Infrastructure/Views")
-                ->set_template("404")
-                ->render_nl();
+                ->set_layout("open/error")
+                ->add_var(PageType::TITLE, $title = __("Terms & Conditions error!"))
+                ->add_var(PageType::H1, $title)
+                ->add_var("error", $e->getMessage())
+                ->add_var("code", $e->getCode())
+                ->render();
         }
         catch (ForbiddenException $e) {
             $this->add_header(ResponseType::FORBIDDEN)
-                ->add_var(PageType::H1, $e->getMessage())
-                ->set_foldertpl("Open/Errors/Infrastructure/Views")
-                ->set_template("403")
+                ->add_var(PageType::TITLE, $title = __("Terms & Conditions error!"))
+                ->add_var(PageType::H1, $title)
+                ->add_var("error", $e->getMessage())
+                ->add_var("code", $e->getCode())
                 ->render_nl();
         }
         catch (Exception $e) {
             $this->add_header(ResponseType::INTERNAL_SERVER_ERROR)
-                ->add_var(PageType::H1, $e->getMessage())
-                ->set_foldertpl("Open/Errors/Infrastructure/Views")
-                ->set_template("500")
-                ->render_nl();
+                ->set_layout("open/error")
+                ->add_var(PageType::TITLE, $title = __("Terms & Conditions error!"))
+                ->add_var(PageType::H1, $title)
+                ->add_var("error", $e->getMessage())
+                ->add_var("code", $e->getCode())
+                ->render();
         }
     }
-
-
 }
 
 
