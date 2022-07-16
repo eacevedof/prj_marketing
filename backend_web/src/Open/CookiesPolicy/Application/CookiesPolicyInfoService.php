55036a4006e66faa45084fc07a9d1d5fd6f6735d
ioedumac <eacevedof@yahoo.es>
2022-07-16 11:41:16 +0200

    policy

--- backend_web/src/Open/CookiesPolicy/Application/CookiesPolicyInfoService.php
+++ backend_web/src/Open/CookiesPolicy/Application/CookiesPolicyInfoService.php
@@ -101,7 +101,9 @@ final class CookiesPolicyInfoService extends AppService
             ["h2" => __("6.- Acceptance, configuration or rejection of cookies, and more information")],
             ["p" => __("In the cookie banner, you can click the “ACCEPT ALL” button to accept all cookies. Likewise, you can click on “ACCEPT SELECTION“ to accept only the cookies marked in the configuration boxes available in the banner, or you can click on “REJECT“ to reject unnecessary cookies.")],
             ["p" => __("We inform you that in the case of blocking or not accepting the installation of cookies, it is possible that certain services will not be available without the use of cookies or that you will not be able to access certain services or take full advantage of everything that this Website offers you.")],
-            ["p" => __("<a href=\"javascript: Cookiebot.renew()\">Change your consent</a>You can obtain more information about the processing of your data by contacting <a href=\"mailto:data.protection.office@nttdata.com\">data.protection.office@nttdata.com</a>")],
+            ["p" => __("<a href=\"javascript: Cookiebot.renew()\">Change your consent</a>")],
+            ["p" => __("You can obtain more information about the processing of your data by contacting <a href=\"mailto:info@providerxxx.es\">info@providerxxx.es</a>")],
+            
         ];
     }
 }
