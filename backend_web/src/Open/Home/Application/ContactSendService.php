86a8e6096da2dd1cdd4f3115107ff2410ffcf7a6
ioedumac <eacevedof@yahoo.es>
2022-07-17 19:40:45 +0200

    send contact

--- backend_web/src/Open/Home/Application/ContactSendService.php
+++ backend_web/src/Open/Home/Application/ContactSendService.php
@@ -2,6 +2,7 @@
 namespace App\Open\Home\Application;
 
 use App\Checker\Application\CheckerService;
+use App\Shared\Infrastructure\Exceptions\FieldsException;
 use App\Shared\Infrastructure\Services\AppService;
 use App\Shared\Infrastructure\Factories\Specific\ValidatorFactory as VF;
 use App\Shared\Domain\Entities\FieldsValidator;
@@ -19,6 +20,7 @@ final class ContactSendService extends AppService
             "subject" => trim($input["subject"] ?? ""),
             "message" => trim($input["message"] ?? ""),
         ];
+        $this->_load_validator();
     }
 
     private function _load_validator(): void
@@ -52,15 +54,19 @@ final class ContactSendService extends AppService
                 $value = $data["value"];
                 if (!$value)
                     return __("Empty value is not allowed");
-                if (strlen($value)<10 || strlen($value)>50)
-                    return __("{0} must be greater than {1} and lighter than {2}", __("Email"), 10, 50);
-            })
-        ;
+                if (strlen($value)<10 || strlen($value)>2000)
+                    return __("{0} must be greater than {1} and lighter than {2}", __("Email"), 10, 2000);
+            });
     }
 
-
-    public function __invoke(): void
+    public function __invoke(): array
     {
-
+        if ($errors = $this->validator->get_errors()) {
+            $this->_set_errors($errors);
+            throw new FieldsException(__("Fields validation errors"));
+        }
+        return [
+            "email" => $this->input["email"],
+        ];
     }
 }
