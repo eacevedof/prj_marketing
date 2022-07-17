85a5778f51d60285c2631235f95d1e1552815968
ioedumac <eacevedof@yahoo.es>
2022-07-17 18:40:14 +0200

    send contact

--- backend_web/src/Open/Home/Infrastructure/Controllers/ContactSendController.php
+++ backend_web/src/Open/Home/Infrastructure/Controllers/ContactSendController.php
@@ -1,18 +1,49 @@
 <?php
 namespace App\Open\Home\Infrastructure\Controllers;
 
+use App\Open\PromotionCaps\Domain\Enums\RequestActionType;
+use App\Shared\Domain\Enums\ResponseType;
 use App\Shared\Infrastructure\Controllers\Open\OpenController;
-use App\Shared\Domain\Enums\PageType;
+use App\Shared\Infrastructure\Exceptions\FieldsException;
+use App\Open\Home\Application\ContactSendService;
+use App\Shared\Infrastructure\Factories\ServiceFactory as SF;
 
 final class ContactSendController extends OpenController
 {
     public function send(): void
     {
-        $this->set_layout("open/mypromos/home")
-            ->add_var(PageType::TITLE, $title = __("My Promotions"))
-            ->add_var(PageType::H1, $title)
-            //->cache()
-            ->render();
+        if (!$this->request->is_accept_json())
+            $this->_get_json()
+                ->set_code(ResponseType::BAD_REQUEST)
+                ->set_error([__("Only type json for accept header is allowed")])
+                ->show();
+
+        $post = $this->request->get_post();
+        if (($post["_action"] ?? "") !== RequestActionType::HOME_CONTACT_SEND)
+            $this->_get_json()
+                ->set_code(ResponseType::BAD_REQUEST)
+                ->set_error([__("Wrong action")])
+                ->show();
+
+        $send = SF::get_callable(ContactSendService::class, $post);
+        try {
+            $result = $send();
+            $this->_get_json()->set_payload([
+                "message" => $result["description"],
+            ])->show();
+        }
+        catch (FieldsException $e) {
+            $this->_get_json()->set_code($e->getCode())
+                ->set_error([
+                    ["fields_validation" => $send->get_errors()]
+                ])
+                ->show();
+        }
+        catch (Exception $e) {
+            $this->_get_json()->set_code($e->getCode())
+                ->set_error([$e->getMessage()])
+                ->show();
+        }
     }
 }
 
--- backend_web/src/Open/PromotionCaps/Domain/Enums/RequestActionType.php
+++ backend_web/src/Open/PromotionCaps/Domain/Enums/RequestActionType.php
@@ -4,12 +4,14 @@ namespace App\Open\PromotionCaps\Domain\Enums;
 
 abstract class RequestActionType
 {
-    const PROMOTIONCAP_INSERT = "promotioncap.insert";
+    public const PROMOTIONCAP_INSERT = "promotioncap.insert";
+    public const HOME_CONTACT_SEND = "home.contact.send";
 
     public static function get_all(): array
     {
         return [
             self::PROMOTIONCAP_INSERT,
+            self::HOME_CONTACT_SEND,
         ];
     }
 }
