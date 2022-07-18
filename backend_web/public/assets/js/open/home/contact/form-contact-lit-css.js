aa85dc6af283b71db4c402ae5d71a69b140bbddb
ioedumac <eacevedof@yahoo.es>
2022-07-18 20:48:40 +0200

    top error

--- backend_web/public/assets/js/open/home/contact/form-contact-lit-css.js
+++ backend_web/public/assets/js/open/home/contact/form-contact-lit-css.js
@@ -29,6 +29,15 @@ export const cssformcontact = css`
   resize: none;
 }
 
+.form-grid .error-top {
+  background: white;
+  align-self: center;
+  border: 1px solid red;
+  padding: 3px;
+  padding-bottom: 6px;
+  border-radius: 5px;
+}
+
 .form-grid .cell-flex div[approle=field-error] ul {
   display: flex;
   list-style-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='none' stroke='%23dc3545'%3e%3ccircle cx='6' cy='6' r='4.5'/%3e%3cpath stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/%3e%3ccircle cx='6' cy='8.2' r='.6' fill='%23dc3545' stroke='none'/%3e%3c/svg%3e");
