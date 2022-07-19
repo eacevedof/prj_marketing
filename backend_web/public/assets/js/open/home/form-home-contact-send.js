c3d95b5a955dcab2bcad89fd5888e49f4b9732b3
ioedumac <eacevedof@yahoo.es>
2022-07-19 19:21:47 +0200

    form-home-contact-send

--- backend_web/public/assets/js/open/home/form-home-contact-send.js
+++ backend_web/public/assets/js/open/home/form-home-contact-send.js
@@ -3,7 +3,7 @@ import injson from "/assets/js/common/req.js"
 import error from "/assets/js/common/fielderrors.js"
 import {cssfielderror} from "/assets/js/common/fielderrors-lit-css.js"
 import {selector, get_formdata} from "/assets/js/common/shadowroot/shadowroot.js"
-import {cssformcontact} from "/assets/js/open/home/contact/form-home-contact-send-lit-css.js"
+import {cssformcontact} from "/assets/js/open/home/form-home-contact-send-lit-css.js"
 import validator, {PATTERNS} from "/assets/js/common/fields-validator.js"
 
 const URL_POST = "/contact/send"
