94a5be35da50ddb216dc2ca300630e4af8a9cac8
ioedumac <eacevedof@yahoo.es>
2022-07-31 18:08:13 +0200

    fix po cancel

--- backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapCancelController.php
+++ backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapCancelController.php
@@ -4,28 +4,32 @@
  */
 namespace App\Open\PromotionCaps\Infrastructure\Controllers;
 
-use App\Shared\Domain\Enums\ResponseType;
 use App\Shared\Infrastructure\Controllers\Open\OpenController;
 use App\Shared\Infrastructure\Factories\ServiceFactory as SF;
+use App\Open\Business\Application\BusinessSpaceService;
 use App\Open\PromotionCaps\Application\PromotionCapsCancelService;
+use App\Shared\Domain\Enums\ResponseType;
 use App\Shared\Domain\Enums\PageType;
 use App\Open\PromotionCaps\Domain\Errors\PromotionCapException;
 
 final class PromotionCapCancelController extends OpenController
 {
-    public function cancel(string $promotionuuid, string $subscriptionuuid): void
+    public function cancel(string $businessslug, string $subscriptionuuid): void
     {
-        if (!($promotionuuid && $subscriptionuuid))
+        if (!($businessslug && $subscriptionuuid))
             $this->add_header(ResponseType::BAD_REQUEST)
                 ->set_layout("open/mypromos/error")
                 ->add_var(PageType::TITLE, $title = __("Subscription cancellation error!"))
                 ->add_var(PageType::H1, $title)
-                ->add_var("error", __("Missing promotion and/or subscription code"))
+                ->add_var("error", __("Missing partner and/or subscription code"))
                 ->add_var("code", ResponseType::BAD_REQUEST)
                 ->render_nv();
+
+        $istest = ($this->request->get_get("mode", "")==="test");
+        $space = SF::get(BusinessSpaceService::class, ["_test_mode" => $istest])->get_data_by_promocap($subscriptionuuid);
         try {
             $insert = SF::get_callable(PromotionCapsCancelService::class, [
-                "promotionuuid" => $promotionuuid,
+                "promotionuuid" => $space["promocode"] ?? "",
                 "subscriptionuuid" => $subscriptionuuid,
                 "_test_mode" => $this->request->get_get("mode", "")==="test",
             ]);
@@ -33,6 +37,7 @@ final class PromotionCapCancelController extends OpenController
             $this->set_layout("open/mypromos/success")
                 ->add_var(PageType::TITLE, $title = __("Subscription cancellation successfully done!"))
                 ->add_var(PageType::H1, $title)
+                ->add_var("space", $space)
                 ->add_var("success",  [
                     ["p" => __("<b>{0}</b>. You have successfully cancelled your subscription to <b>“{1}“</b>", $result["username"], $result["promotion"])],
                 ]);
@@ -44,6 +49,7 @@ final class PromotionCapCancelController extends OpenController
                 ->set_layout("open/mypromos/error")
                 ->add_var(PageType::TITLE, $title = __("Subscription cancellation error!"))
                 ->add_var(PageType::H1, $title)
+                ->add_var("space", $space)
                 ->add_var("error", $e->getMessage())
                 ->add_var("code", $e->getCode())
                 ->render_nv();
@@ -53,6 +59,7 @@ final class PromotionCapCancelController extends OpenController
                 ->set_layout("open/mypromos/error")
                 ->add_var(PageType::TITLE, $title = __("Subscription cancellation error!"))
                 ->add_var(PageType::H1, $title)
+                ->add_var("space", $space)
                 ->add_var("error", $e->getMessage())
                 ->add_var("code", $e->getCode())
                 ->render_nv();
--- backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapConfirmController.php
+++ backend_web/src/Open/PromotionCaps/Infrastructure/Controllers/PromotionCapConfirmController.php
@@ -21,7 +21,7 @@ final class PromotionCapConfirmController extends OpenController
                 ->add_header($code = ResponseType::BAD_REQUEST)
                 ->add_var(PageType::TITLE, $title = __("Subscription confirmation error!"))
                 ->add_var(PageType::H1, $title)
-                ->add_var("error", __("Missing promotion and/or subscription code"))
+                ->add_var("error", __("Missing partner and/or subscription code"))
                 ->add_var("code", $code)
                 ->render_nv();
 
