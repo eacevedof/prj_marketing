ec5c0715b3aeb899ad57e9db0a2fd513a4e05466
ioedumac <eacevedof@yahoo.es>
2022-07-31 17:37:00 +0200

    fix accumulated points

--- backend_web/src/Open/UserCaps/Infrastructure/Controllers/UserCapPointsController.php
+++ backend_web/src/Open/UserCaps/Infrastructure/Controllers/UserCapPointsController.php
@@ -36,7 +36,7 @@ final class UserCapPointsController extends OpenController
             $result = $business();
 
             $title = htmlentities($result["business_name"] ?? "");
-            $title = __("Accumulated points of {0} at {1}", $result["username"] ?? $result["email"], $title);
+            $title = __("Accumulated points of <u>{0}</u> at “{1}“", $result["username"] ?? $result["email"], $title);
             $this->set_layout("open/mypromos/success")
                 ->add_var(PageType::TITLE, $title)
                 ->add_var(PageType::H1, $title)
