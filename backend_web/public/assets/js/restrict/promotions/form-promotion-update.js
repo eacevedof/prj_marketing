741610cff3038bea83aee7b5de4c939d86ce3d63
ioedumac <eacevedof@yahoo.es>
2022-07-31 08:03:44 +0200

    routes

--- backend_web/public/assets/js/restrict/promotions/form-promotion-update.js
+++ backend_web/public/assets/js/restrict/promotions/form-promotion-update.js
@@ -196,7 +196,7 @@ export class FormPromotionUpdate extends LitElement {
             <label>${this.texts.f06}: </label>
             <span>
             ${html([
-              get_link_local(this._promotionlink.concat("/").concat(this._slug.concat("?mode=test")), this._slug)
+              get_link_local(this._promotionlink.concat("?mode=test"), this._slug)
             ])}
             </span>
           </div>
