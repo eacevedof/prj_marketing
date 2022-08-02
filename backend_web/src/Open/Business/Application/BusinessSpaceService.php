1fbcb6d22dc4d12f5bb47adb197ca3582271ee4b
ioedumac <eacevedof@yahoo.es>
2022-08-02 20:20:30 +0200

    fix slug

--- backend_web/src/Open/Business/Application/BusinessSpaceService.php
+++ backend_web/src/Open/Business/Application/BusinessSpaceService.php
@@ -70,8 +70,10 @@ final class BusinessSpaceService extends AppService
     {
         $bd = RF::get(BusinessDataRepository::class)->get_by_slug(
             $businessslug,
-            ["business_name", "url_business", "url_favicon", "user_logo_1", "url_social_fb", "url_social_ig",
-                "url_social_twitter", "url_social_tiktok","body_bgimage"]
+            [
+                "slug", "business_name", "url_business", "url_favicon", "user_logo_1", "url_social_fb", "url_social_ig",
+                "url_social_twitter", "url_social_tiktok","body_bgimage"
+            ]
         );
         if (!$bd) {
             throw new NotFoundException(__("Partner “{0}“ not found!", $businessslug));
@@ -81,6 +83,7 @@ final class BusinessSpaceService extends AppService
             "business" => $bd["business_name"],
             //"businessurl" => Routes::url("business.space", ["businessslug"=>$bd["slug"]]),
             //quiza conviene usar la url de su sitio original en el logo y en los restantes usar el del espacio
+            "businessslug" => $bd["slug"],
             "businessfavicon" => $bd["url_favicon"],
             "businessurl" => $bd["url_business"],
             "businesslogo" => $bd["user_logo_1"],
