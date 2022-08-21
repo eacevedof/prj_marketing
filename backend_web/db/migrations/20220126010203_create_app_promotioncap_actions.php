59393a8d70e3ff3ab962d89a9d4d8ec6e90d1e52
ioedumac <eacevedof@yahoo.es>
2022-08-21 13:06:19 +0200

    fix remote_ip length

--- backend_web/db/migrations/20220126010203_create_app_promotioncap_actions.php
+++ backend_web/db/migrations/20220126010203_create_app_promotioncap_actions.php
@@ -56,7 +56,7 @@ final class CreateAppPromotioncapActions extends AbsMigration
             "comment" => "0:No, 1:Yes"
         ])
         ->addColumn("remote_ip", "string", [
-            "limit" => 15,
+            "limit" => 50,
             "null" => true,
             "default" => null
         ])
