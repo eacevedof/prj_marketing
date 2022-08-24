530ac89fdf6c6b97b8cefaa95ae2272345b962dc
ioedumac <eacevedof@yahoo.es>
2022-08-24 21:30:22 +0200

    link.ts

--- backend_web/public/assets/js/common/html/link.js
+++ backend_web/public/assets/js/common/html/link.js
@@ -1,24 +1,26 @@
 export const get_link_local = (urlhref, text) => {
-  let url = urlhref.trim()
-  if (!url) return ""
-  if (!url.startsWith("/")) url = "/".concat(url)
-  return `<a href="${url}" target="_blank" class="link-info">${text}</a>`
-}
-
+    let url = urlhref.trim();
+    if (!url)
+        return "";
+    if (!url.startsWith("/"))
+        url = "/".concat(url);
+    return `<a href="${url}" target="_blank" class="link-info">${text}</a>`;
+};
 export const get_link = (urlhref, text) => {
-  const url = urlhref.trim()
-  if (!url) return ""
-  if (!(url.includes("https://") || url.includes("http://"))) return ""
-
-  return `<a href="${url}" target="_blank" class="link-info">${text}</a>`
-}
-
+    const url = urlhref.trim();
+    if (!url)
+        return "";
+    if (!(url.includes("https://") || url.includes("http://")))
+        return "";
+    return `<a href="${url}" target="_blank" class="link-info">${text}</a>`;
+};
 export const get_img_link = (urlhref) => {
-  const url = urlhref.trim()
-  if (!url) return ""
-  if (!(url.includes("https://") || url.includes("http://"))) return ""
-
-  return `<a href="${url}" target="_blank" class="link-info">
+    const url = urlhref.trim();
+    if (!url)
+        return "";
+    if (!(url.includes("https://") || url.includes("http://")))
+        return "";
+    return `<a href="${url}" target="_blank" class="link-info">
       <img src="${url}" class="img-thumbnail wd-30p">
-    </a>`
-}
+    </a>`;
+};
