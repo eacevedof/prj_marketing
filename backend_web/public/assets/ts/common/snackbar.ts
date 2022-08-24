d6db021d121d867de3c6f470a00d3ab8c6f414e9
ioedumac <eacevedof@yahoo.es>
2022-08-24 22:55:28 +0200

    snackbar.ts

--- backend_web/public/assets/ts/common/snackbar.ts
+++ backend_web/public/assets/ts/common/snackbar.ts
@@ -1,43 +1,49 @@
-export const SNACK = {
-  ERROR: "#ee335e",
-  SUCCESS: "#22c03c",
+export enum SNACK  {
+  ERROR = "#ee335e",
+  SUCCESS = "#22c03c",
 }
 
-function Snackbar(id) {
-  const _id = id
-  let _time = 3
+function Snackbar(id: string):any {
+  const _id:string = id
+  let _time:number = 3
 
-  const _$div = document.getElementById(_id)
+  const _$div: HTMLElement | null = document.getElementById(_id)
   if(!_$div) return console.log("no snackbar found with id",_id)
   //css: common/snackbar.css
   _$div.classList.add("snackbar")
 
-  this.set_inner = html => {
+  // @ts-ignore
+  this.set_inner = (html: string): any => {
     _$div.innerHTML = html
+    // @ts-ignore
     return this
   }
 
-  this.set_color = (back, front) => {
+  // @ts-ignore
+  this.set_color = (back:string, front:string): any => {
     if (back) _$div.style.backgroundColor = back
     if (front) _$div.style.color = front
+    // @ts-ignore
     return this
   }
 
-  const _set_animation = (hold, time) => {
+  const _set_animation = (hold:number, time:number): any => {
     if (!time) time = 0.5
-    const _hold = hold ? hold - (time * 2): 2.5
+    const _hold:number = hold ? hold - (time * 2): 2.5
     //-webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
     //animation: fadein 0.5s, fadeout 0.5s 2.5s;
     //console.log("time:",time,"_hold:",_hold)
-    const animation = `snackframe-fadein ${time}s, snackframe-fadeout ${time}s ${_hold}s`
-    _$div.style.webkitAnimationName = animation
+    const animation: string = `snackframe-fadein ${time}s, snackframe-fadeout ${time}s ${_hold}s`
     _$div.style.animation = animation
+    // @ts-ignore
     return this
   }
 
-  this.set_time = time => {
+  // @ts-ignore
+  this.set_time = (time: number):any => {
     _time = time
     //console.log("_time set_time", _time)
+    // @ts-ignore
     return this
   }
 
