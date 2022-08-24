33486719c37e761be06abd5dce1fc9e3b9d27919
ioedumac <eacevedof@yahoo.es>
2022-08-24 21:29:19 +0200

    utils.ts

--- backend_web/public/assets/ts/common/html/link.ts
+++ backend_web/public/assets/ts/common/html/link.ts
@@ -1,11 +1,11 @@
-export const get_link_local = (urlhref, text) => {
-  let url = urlhref.trim()
+export const get_link_local = (urlhref: string, text:string): string => {
+  let url: string = urlhref.trim()
   if (!url) return ""
   if (!url.startsWith("/")) url = "/".concat(url)
   return `<a href="${url}" target="_blank" class="link-info">${text}</a>`
 }
 
-export const get_link = (urlhref, text) => {
+export const get_link = (urlhref: string, text:string): string => {
   const url = urlhref.trim()
   if (!url) return ""
   if (!(url.includes("https://") || url.includes("http://"))) return ""
@@ -13,7 +13,7 @@ export const get_link = (urlhref, text) => {
   return `<a href="${url}" target="_blank" class="link-info">${text}</a>`
 }
 
-export const get_img_link = (urlhref) => {
+export const get_img_link = (urlhref: string): string => {
   const url = urlhref.trim()
   if (!url) return ""
   if (!(url.includes("https://") || url.includes("http://"))) return ""
