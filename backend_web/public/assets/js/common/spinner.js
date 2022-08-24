2560aad07d4a035143cd10b1183e114b758c235b
ioedumac <eacevedof@yahoo.es>
2022-08-24 23:07:23 +0200

    spinner.ts

--- backend_web/public/assets/js/common/spinner.js
+++ backend_web/public/assets/js/common/spinner.js
@@ -1,35 +1,35 @@
-import {load_asset_css} from "/assets/js/common/utils.js"
-const PATH = "common/spinner"
-load_asset_css([PATH])
-let $wrapper = null
-
-const spinnertpl = `    
+// @ts-ignore
+import { load_asset_css } from "/assets/js/common/utils.js";
+const PATH = "common/spinner";
+load_asset_css([PATH]);
+let $wrapper = null;
+const spinnertpl = `
 <div class="spinner" approle="spinner">
     <div class="spinner-loader"></div>
 </div>
-`
-
+`;
 const _remove_spinner = () => {
-  const $spinner = $wrapper.querySelector(`[approle="spinner"]`)
-  if($spinner) $spinner.parentNode.removeChild($spinner)
-}
-
+    var _a;
+    const $spinner = $wrapper === null || $wrapper === void 0 ? void 0 : $wrapper.querySelector(`[approle="spinner"]`);
+    if ($spinner)
+        (_a = $spinner === null || $spinner === void 0 ? void 0 : $spinner.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild($spinner);
+};
 const _add_spinner = () => {
-  $wrapper.innerHTML = ""
-  $wrapper.innerHTML = spinnertpl
-}
-
-const _render_spinner = $cont => {
-  if(!$cont) $cont = document.getElementById("spinner-global")
-  if(!$cont) return
-  $wrapper = $cont
-  _remove_spinner()
-  _add_spinner()
-}
-
+    if (!$wrapper)
+        return;
+    $wrapper.innerHTML = "";
+    $wrapper.innerHTML = spinnertpl;
+};
+const _render_spinner = ($cont) => {
+    if (!$cont)
+        $cont = document.getElementById("spinner-global");
+    if (!$cont)
+        return;
+    $wrapper = $cont;
+    _remove_spinner();
+    _add_spinner();
+};
 export default {
-  render: _render_spinner,
-  remove: _remove_spinner,
-}
-
-
+    render: _render_spinner,
+    remove: _remove_spinner,
+};
--- backend_web/public/assets/ts/common/spinner.ts
+++ backend_web/public/assets/ts/common/spinner.ts
@@ -1,25 +1,27 @@
+// @ts-ignore
 import {load_asset_css} from "/assets/js/common/utils.js"
-const PATH = "common/spinner"
+const PATH:string = "common/spinner"
 load_asset_css([PATH])
-let $wrapper = null
+let $wrapper:HTMLElement|null = null
 
-const spinnertpl = `    
+const spinnertpl: string = `
 <div class="spinner" approle="spinner">
     <div class="spinner-loader"></div>
 </div>
 `
 
-const _remove_spinner = () => {
-  const $spinner = $wrapper.querySelector(`[approle="spinner"]`)
-  if($spinner) $spinner.parentNode.removeChild($spinner)
+const _remove_spinner = (): void => {
+  const $spinner: HTMLElement|null|undefined = $wrapper?.querySelector(`[approle="spinner"]`)
+  if($spinner) $spinner?.parentNode?.removeChild($spinner)
 }
 
-const _add_spinner = () => {
+const _add_spinner = (): void => {
+  if (!$wrapper) return
   $wrapper.innerHTML = ""
   $wrapper.innerHTML = spinnertpl
 }
 
-const _render_spinner = $cont => {
+const _render_spinner = ($cont: HTMLElement|null) => {
   if(!$cont) $cont = document.getElementById("spinner-global")
   if(!$cont) return
   $wrapper = $cont
