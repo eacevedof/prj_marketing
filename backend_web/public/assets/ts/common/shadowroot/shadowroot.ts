fa4406d5abd13664ec02a6b74341fac29e38e013
ioedumac <eacevedof@yahoo.es>
2022-08-26 22:40:18 +0200

    ts

--- backend_web/public/assets/ts/common/shadowroot/shadowroot.ts
+++ backend_web/public/assets/ts/common/shadowroot/shadowroot.ts
@@ -2,16 +2,19 @@ type FnShadowSelector = (selector:string) => HTMLElement | null
 
 export const selector = (shadow:ShadowRoot): FnShadowSelector => (strsel: string):HTMLElement|null => shadow.querySelector(`#${strsel}`)
 
+type FnExcludeFromForm = (exclude:string[]) => Object
+type FnAllFieldIds = (fields:string[] | Object) => FnExcludeFromForm
+
 //arexclude evita que se envie esa key
-export const get_formdata = (shadow:ShadowRoot) => (fields:string[]| Object) => (arexclude:string[]=[]): string[] => {
+export const get_formdata = (shadow:ShadowRoot):FnAllFieldIds => (fields:string[]| Object):FnExcludeFromForm => (arexclude:string[]=[]): Object => {
   const fieldids: string[] = Array.isArray(fields) ? fields : Object.keys(fields)
-  const data = fieldids.map((field:string): Object => {
+  const data: Object = fieldids.map((field:string): Object => {
       if (arexclude.includes(field)) return {}
       const found: HTMLElement | null = selector(shadow)(field)
 
       const ob:Record<string, string> = {}
-
-      ob.field = (found?.getAttribute("type") === "checkbox") ?
+      //guardo un objeto con field: value
+      ob[field] = (found?.getAttribute("type") === "checkbox") ?
                     (found.getAttribute("checked") ?
                         (found.getAttribute("value") ?? "1") : "0"
                     ) :
