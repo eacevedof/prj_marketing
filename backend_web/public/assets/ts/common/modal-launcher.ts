4b872e209299e3651b31f30d8b113d0c5da34ef7
ioedumac <eacevedof@yahoo.es>
2022-08-26 21:08:48 +0200

    ts

--- backend_web/public/assets/ts/common/modal-launcher.ts
+++ backend_web/public/assets/ts/common/modal-launcher.ts
@@ -5,13 +5,21 @@ import spinner from "/assets/js/common/spinner.js"
 // @ts-ignore
 import {SNACK} from "/assets/js/common/snackbar.js"
 
+declare global {
+  interface Window {
+    snack: any,
+    modalraw: any
+  }
+}
+
 const _open_modal_by_get = async (url: string): Promise<void> => {
   spinner.render()
   const r = await reqtxt.get(url)
   spinner.remove()
   if (r.errors)
-    return window?.snack.set_color(SNACK.ERROR).set_time(5).set_inner(r.errors[0]).show()
-  window?.modalraw.opts({
+    return window.snack.set_color(SNACK.ERROR).set_time(5).set_inner(r.errors[0]).show()
+
+  window.modalraw.opts({
     bgclick: false,
     body: r,
   }).show()
@@ -20,20 +28,20 @@ const _open_modal_by_get = async (url: string): Promise<void> => {
 const VIEWS = ["info", "edit"]
 
 //modal_launcher()
-export default () => {
-  const url = window.location.search;
+export default ():void => {
+  const url: string = window.location.search
   if (!url) return
-  const urlparams = new URLSearchParams(url)
+  const urlparams:URLSearchParams = new URLSearchParams(url)
 
-  const uuid = urlparams.get("uuid")
+  const uuid: string|null = urlparams.get("uuid")
   if (!uuid) return
 
-  const view = urlparams.get("view")?.trim() ?? "info"
+  const view: string = urlparams.get("view")?.trim() ?? "info"
   if (!VIEWS.includes(view)) return
 
-  const tab = urlparams.get("tab")?.trim() ?? "main"
-  const pathname = window.location.pathname.split("/").slice(0,-1).join("/")
-  let final = pathname.concat(`/${view}`).concat(`/${uuid}`).concat(`?tab=${tab}`)
+  const tab: string = urlparams.get("tab")?.trim() ?? "main"
+  const pathname: string = window.location.pathname.split("/").slice(0,-1).join("/")
+  let final: string = pathname.concat(`/${view}`).concat(`/${uuid}`).concat(`?tab=${tab}`)
   //http://localhost:900/restrict/users/info/620d471857bc
   //http://localhost:900/restrict/users/edit/620d471857bc4
   //alert(final)
@@ -42,7 +50,7 @@ export default () => {
 }
 
 export const show_restrict_url = async () => {
-  const is_valid_url = async url => {
+  const is_valid_url = async (url: string): Promise<boolean> => {
     if (url.match(/\/restrict[\/]*$/)) return false
     const is200 = await is_get_200(url)
     if (url.startsWith("/restrict/") && is200)
