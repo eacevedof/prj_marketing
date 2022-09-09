c170b5a2029287afec2712640061d2823982c783
ioedumac <eacevedof@yahoo.es>
2022-09-09 11:06:23 +0200

    migratio

--- backend_web/db/migrations/20220910010203_add_raffle_configuration.php
+++ backend_web/db/migrations/20220910010203_add_raffle_configuration.php
@@ -2,26 +2,19 @@
 declare(strict_types=1);
 use Migrations\AbsMigration;
 
-final class AddRaffleConfiguraton extends AbsMigration
+final class AddRaffleConfiguration extends AbsMigration
 {
     private string $tablename = "app_promotion";
 
     public function up(): void
     {
-        $this->_create_table();
+        $this->_add_columns();
         $this->_initial_load();
     }
 
-    private function _create_table(): void
+    private function _add_columns(): void
     {
-        $table = $this->table("{$this->tablename}", [
-            "engine" => "MyISAM",
-            "collation" => "utf8_general_ci",
-            "id"=> false,
-            "primary_key" => ["id"]
-        ]);
-
-        $this->add_sysfields($table);
+        $table = $this->table("{$this->tablename}");
 
         $table->addColumn("id", "integer", [
             "limit" => 11,
@@ -35,174 +28,10 @@ final class AddRaffleConfiguraton extends AbsMigration
             "limit" => 11,
             "null" => false
         ])
-        ->addColumn("id_tz", "integer", [
-            "limit" => 5,
-            "null" => false,
-            "default" => 1 //app_array.tz 1 = UTC
-        ])
-        ->addColumn("code_erp", "string", [
-            "limit" => 25,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("description", "string", [
-            "limit" => 250,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("slug", "string", [
-            "limit" => 250,
-            "null" => false,
-        ])
-        ->addColumn("date_from", "datetime", [
-            "null" => false,
-        ])
-        ->addColumn("date_to", "datetime", [
-            "null" => false,
-        ])
-        ->addColumn("date_execution", "datetime", [
-            "null" => false,
-            "comment" => "día y hora limite para validar el codigo",
-        ])
-        ->addColumn("content", "string", [
-            "limit" => 2000,
-            "null" => false,
-        ])
-        ->addColumn("bgcolor", "string", [
-            "limit" => 10,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("bgimage_xs", "string", [
-            "limit" => 500,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("bgimage_sm", "string", [
-            "limit" => 500,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("bgimage_md", "string", [
-            "limit" => 500,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("bgimage_lg", "string", [
-            "limit" => 500,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("bgimage_xl", "string", [
-            "limit" => 500,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("bgimage_xxl", "string", [
-            "limit" => 500,
-            "null" => true,
-            "default" => null,
-        ])
-        ->addColumn("invested", "decimal", [
-            "null" => true,
-            "default" => 0,
-            "precision" => 10,
-            "scale" => 3
-        ])
-        ->addColumn("returned", "decimal", [
-            "null" => true,
-            "default" => 0,
-            "precision" => 10,
-            "scale" => 3
-        ])
-        ->addColumn("max_confirmed", "integer", [
-            "limit" => 4,
-            "null" => false,
-            "default" => 0,
-        ])
-        ->addColumn("is_raffleable", "integer", [
-            "limit" => 2,
-            "null" => false,
-            "default" => 0,
-        ])
-        ->addColumn("is_cumulative", "integer", [
-            "limit" => 2,
-            "null" => false,
-            "default" => 0,
-        ])
-        ->addColumn("tags", "string", [
-            "limit" => 500,
-            "null" => true,
-            "default" => null,
-            "comment" => "palabras claves en los textos para poder hacer IA"
-        ])
-        ->addColumn("notes", "string", [
-            "limit" => 300,
-            "null" => true,
-            "default" => null,
-            "comment" => "notas de usuario sobre la promocion"
-        ])
-        ->addColumn("is_published", "integer", [
-            "limit" => 2,
-            "null" => false,
-            "default" => 0,
-        ])
-        ->addColumn("is_launched", "integer", [
-            "limit" => 2,
-            "null" => false,
-            "default" => 0,
-            "comment" => "si ya se ha publicado al menos una vez no se deberia poder editar cierta configuracion"
-        ])
-        ->addColumn("num_viewed", "integer", [
-            "limit" => 5,
-            "null" => false,
-            "default" => 0,
-            "comment" => "metricas de trazabilidad, veces vista"
-        ])
-        ->addColumn("num_subscribed", "integer", [
-            "limit" => 5,
-            "null" => false,
-            "default" => 0
-        ])
-        ->addColumn("num_confirmed", "integer", [
-            "limit" => 5,
-            "null" => false,
-            "default" => 0
-        ])
-        ->addColumn("num_executed", "integer", [
-            "limit" => 5,
-            "null" => false,
-            "default" => 0
-        ])
-        ->addColumn("disabled_date", "datetime", [
-            "null" => true,
-            "comment" => "datetime de la deshabilitacion",
-        ])
-        ->addColumn("disabled_user", "string", [
-            "limit" => 15,
-            "null" => true,
-            "comment" => "deshabilitador (no necesariamente en base_user) puede ser un proceso ETL",
-        ])
-        ->addColumn("disabled_reason", "string", [
-            "limit" => 1000,
-            "null" => true,
-            "comment" => "why is disabled",
-        ])
-        ->create();
 
-        $table->addIndex(["delete_date"], ["name"=>"delete_date_idx"])
-            ->addIndex(["is_enabled"], ["name"=>"is_enabled_idx"])
-            ->addIndex(["uuid"], ["name"=>"uuid_idx"])
-            ->addIndex(["id_owner"], ["name"=>"id_owner_idx"])
-            ->addIndex(["description"], ["name"=>"description_idx"])
-            ->addIndex(["slug"], ["name"=>"slug_idx"])
-            ->addIndex(["date_from"], ["name"=>"date_from_idx"])
-            ->addIndex(["date_to"], ["name"=>"date_to_idx"])
-            ->addIndex(["disabled_date"], ["name"=>"disabled_date_idx"])
-            ->addIndex(["id","uuid"], ["name"=>"id__uuid_idx"])
-            ->addIndex(["date_from","date_to"], ["name"=>"date_from__date_to_idx"])
-            ->update()
-        ;
+
+        ->save();
+
     }
 
     private function _initial_load(): void { }
