4e060639e093b565ebf90a0e52a5fd4ed79cb2b9
ioedumac <eacevedof@yahoo.es>
2022-09-09 11:32:31 +0200

    migratio

--- backend_web/db/migrations/20220910010203_add_raffle_configuration.php
+++ backend_web/db/migrations/20220910010203_add_raffle_configuration.php
@@ -4,37 +4,45 @@ use Migrations\AbsMigration;
 
 final class AddRaffleConfiguration extends AbsMigration
 {
-    private string $tablename = "app_promotion";
-
     public function up(): void
     {
-        $this->_add_columns();
+        $this->_add_columns_to_app_promotion();
+        $this->_add_columns_to_app_promotion_cap();
         $this->_initial_load();
     }
 
-    private function _add_columns(): void
+    private function _add_columns_to_app_promotion(): void
     {
-        $table = $this->table("{$this->tablename}");
-
-        $table->addColumn("id", "integer", [
-            "limit" => 11,
-            "identity" => true,
-        ])
-        ->addColumn("uuid", "string", [
-            "limit" => 50,
+        $table = $this->table("app_promotion");
+        $table->addColumn("date_raffle", "datetime", [
             "null" => false,
         ])
-        ->addColumn("id_owner", "integer", [
-            "limit" => 11,
-            "null" => false
-        ])
-
-
         ->save();
-
     }
 
-    private function _initial_load(): void { }
+    private function _add_columns_to_app_promotion_cap(): void
+    {
+        $table = $this->table("app_promotion_cap");
+        $table->addColumn("id_raffle", "tinyint", [
+            "limit" => 3,
+            "null" => true,
+            "default" => null,
+            "comment" => "app_array.type=raffle",
+        ])
+        ->save();
+    }
+
+    private function _initial_load(): void 
+    {
+        $sqls = [
+            "INSERT INTO app_array (is_enabled,TYPE,id_pk,description) VALUES ('1','raffle','1','Winner');",
+            "INSERT INTO app_array (is_enabled,TYPE,id_pk,description) VALUES ('1','raffle','2','Alternate 1');",
+            "INSERT INTO app_array (is_enabled,TYPE,id_pk,description) VALUES ('1','raffle','3','Alternate 2');",
+        ];
+
+        foreach ($sqls as $sql)
+            $this->execute($sql);
+    }
 
     public function down(): void
     {
