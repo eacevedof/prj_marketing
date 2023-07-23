adce9c319a449567992c70892e3d1bbdad570030
eacevedof@mbpro2014 <eacevedof@>
2023-07-23 19:15:14 +0200

    fix json_encode _echo_js

--- backend_web/src/Shared/Infrastructure/Views/AppView.php
+++ backend_web/src/Shared/Infrastructure/Views/AppView.php
@@ -204,7 +204,7 @@ final class AppView
 
     private function _echo_jslit($any): void
     {
-        $json = json_encode($any);
+        $json = json_encode($any, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
         echo str_replace("\"","&quot;", $json);
     }
 
